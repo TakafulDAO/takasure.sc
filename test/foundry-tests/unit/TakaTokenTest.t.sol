c4975b4c725307da69a31d5601c883601184fb78
Maikel Ordaz <maikelordaz@gmail.com>
2024-04-12 22:25:33 -0400

    revise: token unit tests

---
+++ test/foundry-tests/unit/TakaTokenTest.t.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {Test, console} from "forge-std/Test.sol";
+import {DeployTakaTokenAndTakasurePool} from "../../../scripts/foundry-deploy/01-taka-token-takasure-pool/DeployTakaTokenAndTakasurePool.s.sol";
+import {HelperConfig} from "../../../scripts/foundry-deploy/HelperConfig.s.sol";
+import {TakaToken} from "../../../contracts/token/TakaToken.sol";
+import {TakasurePool} from "../../../contracts/token/TakasurePool.sol";
+
+contract TakaTokenTest is Test {
+    DeployTakaTokenAndTakasurePool deployer;
+    TakaToken takaToken;
+    TakasurePool takasurePool;
+    HelperConfig config;
+
+    address public user = makeAddr("user");
+
+    uint256 public constant MINT_AMOUNT = 1 ether;
+
+    function setUp() public {
+        deployer = new DeployTakaTokenAndTakasurePool();
+        (takaToken, takasurePool, ) = deployer.run();
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                REVERTS
+    //////////////////////////////////////////////////////////////*/
+
+    function test_mustRevertIfTryToMintToAddressZero() public {
+        vm.prank(address(takasurePool));
+        vm.expectRevert(TakaToken.TakaToken__NotZeroAddress.selector);
+        takaToken.mint(address(0), MINT_AMOUNT);
+    }
+
+    function test_mustRevertIfTryToMintZero() public {
+        vm.prank(address(takasurePool));
+        vm.expectRevert(TakaToken.TakaToken__MustBeMoreThanZero.selector);
+        takaToken.mint(user, 0);
+    }
+
+    function test_mustRevertIfTryToBurnZero() public {
+        vm.prank(address(takasurePool));
+        vm.expectRevert(TakaToken.TakaToken__MustBeMoreThanZero.selector);
+        takaToken.burn(0);
+    }
+
+    function test_mustRevertIfTtryToBurnMoreThanBalance() public {
+        vm.prank(address(takasurePool));
+        vm.expectRevert(TakaToken.TakaToken__BurnAmountExceedsBalance.selector);
+        takaToken.burn(MINT_AMOUNT);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                             MINT FUNCTION
+    //////////////////////////////////////////////////////////////*/
+
+    function test_mint() public {
+        vm.prank(address(takasurePool));
+        takaToken.mint(user, MINT_AMOUNT);
+        assertEq(takaToken.balanceOf(user), MINT_AMOUNT);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                             BURN FUNCTION
+    //////////////////////////////////////////////////////////////*/
+
+    function test_burn() public {
+        uint256 amountToBurn = MINT_AMOUNT / 2;
+
+        vm.startPrank(address(takasurePool));
+        takaToken.mint(address(takasurePool), MINT_AMOUNT);
+
+        takaToken.burn(amountToBurn);
+        vm.stopPrank();
+
+        assertEq(takaToken.balanceOf(address(takasurePool)), MINT_AMOUNT - amountToBurn);
+    }
+}
