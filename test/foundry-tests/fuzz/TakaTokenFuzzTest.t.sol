f9fe9cc8987b43f9b3365ecc11a5f412d790caeb
Maikel Ordaz <maikelordaz@gmail.com>
2024-04-12 22:29:56 -0400

    revise: fuzz token tests

---
+++ test/foundry-tests/fuzz/TakaTokenFuzzTest.t.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {Test, console} from "forge-std/Test.sol";
+import {DeployTakaTokenAndTakasurePool} from "../../../scripts/foundry-deploy/01-taka-token-takasure-pool/DeployTakaTokenAndTakasurePool.s.sol";
+import {HelperConfig} from "../../../scripts/foundry-deploy/HelperConfig.s.sol";
+import {TakaToken} from "../../../contracts/token/TakaToken.sol";
+import {TakasurePool} from "../../../contracts/token/TakasurePool.sol";
+
+contract TakaTokenFuzzTest is Test {
+    DeployTakaTokenAndTakasurePool deployer;
+    TakaToken takaToken;
+    TakasurePool takasurePool;
+    HelperConfig config;
+
+    address public user = makeAddr("user");
+
+    uint256 public constant MINT_AMOUNT = 1 ether;
+
+    function setUp() public {
+        deployer = new DeployTakaTokenAndTakasurePool();
+        (takaToken, takasurePool, ) = deployer.run();
+    }
+
+    function test_fuzz_onlyMinterCanMint(address minter) public {
+        vm.assume(minter != address(takasurePool));
+        vm.prank(minter);
+        vm.expectRevert();
+        takaToken.mint(user, MINT_AMOUNT);
+    }
+
+    function test_fuzz_onlyBurnerCanBurn(address burner) public {
+        vm.assume(burner != address(takasurePool));
+
+        vm.prank(address(takasurePool));
+        takaToken.mint(address(takasurePool), MINT_AMOUNT);
+
+        vm.prank(burner);
+        vm.expectRevert();
+        takaToken.burn(MINT_AMOUNT);
+    }
+}
