be1c551f2926883642ddd4e1139b2c00b17b112f
Maikel Ordaz <maikelordaz@gmail.com>
2024-04-14 10:16:23 -0400

    chore: configs

--- utils/_networks.js
+++ utils/_networks.js
@@ -14,12 +14,16 @@ const isProtocolChain = isMemnet || isFork || isLocalhost || isMainnet || isTest
 const networkConfig = {
     31337: {
         name: "hardhat",
+        // Same as mainnet
+        usdc: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831", // https://developers.circle.com/developer/docs/supported-chains-and-currencies#native-usdc
     },
     42161: {
         name: "mainnet_arbitrum",
+        usdc: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831", // https://developers.circle.com/developer/docs/supported-chains-and-currencies#native-usdc
     },
     421614: {
         name: "testnet_arbitrum_sepolia",
+        usdc: "0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d", // https://developers.circle.com/stablecoins/docs/usdc-on-test-networks
     },
 }
 
