91c4ad6f276ca59a88a2da592db4056236346242
Maikel Ordaz <maikelordaz@gmail.com>
2024-04-13 22:05:21 -0400

    revise: create MembersModule.sol

---
+++ contracts/modules/MembersModule.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: GPL-3.0
+
+/**
+ * @title MembersModule
+ * @author Maikel Ordaz
+ * @dev Users communicate with this module to become members of the DAO. It contains member management
+ *      functionality such as modifying or canceling the policy, updates BM and BMA, remove non active
+ *      members, calculate surplus
+ * @dev Upgradeable contract with UUPS pattern
+ */
+
+pragma solidity 0.8.24;
+
+contract MembersModule {}
