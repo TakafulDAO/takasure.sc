099ccd17bd407194562cefa8fe07580e3f991bfb
Maikel Ordaz <maikelordaz@gmail.com>
2024-04-13 22:20:11 -0400

    revise: set upgradeable feature

--- contracts/modules/MembersModule.sol
+++ contracts/modules/MembersModule.sol
@@ -9,6 +9,17 @@
  * @dev Upgradeable contract with UUPS pattern
  */
 
+import {UUPSUpgradeable, Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
 pragma solidity 0.8.24;
 
-contract MembersModule {}
+contract MembersModule is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+    function initialize() public initializer {
+        __UUPSUpgradeable_init();
+        __Ownable_init(msg.sender);
+    }
+
+    ///@dev required by the OZ UUPS module
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+}
