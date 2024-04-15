d6175c3f8a447ae61d5b3586fc21f55c9905d416
Maikel Ordaz <maikelordaz@gmail.com>
2024-04-15 10:10:14 -0400

    chore: reorder

--- test/foundry-tests/unit/TakasurePoolTest.t.sol
+++ test/foundry-tests/unit/TakasurePoolTest.t.sol
@@ -29,57 +29,6 @@ contract TakasurePoolTest is Test {
     }
 
     /*//////////////////////////////////////////////////////////////
-                                GETTERS
-    //////////////////////////////////////////////////////////////*/
-    function testTakasurePool_membersModuleIsMinterAndBurner() public view {
-        bytes32 MINTER_ROLE = keccak256("MINTER_ROLE");
-        bytes32 BURNER_ROLE = keccak256("BURNER_ROLE");
-
-        bool isMinter = takasurePool.hasRole(MINTER_ROLE, address(membersModule));
-        bool isBurner = takasurePool.hasRole(BURNER_ROLE, address(membersModule));
-        // bool isMinter = takasurePool.hasRole(MINTER_ROLE, address(proxy));
-        // bool isBurner = takasurePool.hasRole(BURNER_ROLE, address(proxy));
-
-        assert(isMinter);
-        assert(isBurner);
-    }
-
-    /*//////////////////////////////////////////////////////////////
-                                REVERTS
-    //////////////////////////////////////////////////////////////*/
-
-    function testTakasurePool_mustRevertIfTryToMintToAddressZero() public {
-        vm.prank(address(membersModule));
-        vm.expectRevert(TakasurePool.TakasurePool__NotZeroAddress.selector);
-        takasurePool.mint(address(0), MINT_AMOUNT);
-    }
-
-    function testTakasurePool_mustRevertIfTryToMintZero() public {
-        vm.prank(address(membersModule));
-        vm.expectRevert(TakasurePool.TakasurePool__MustBeMoreThanZero.selector);
-        takasurePool.mint(user, 0);
-    }
-
-    function testTakasurePool_mustRevertIfTryToBurnFromAddressZero() public {
-        vm.prank(address(membersModule));
-        vm.expectRevert(TakasurePool.TakasurePool__NotZeroAddress.selector);
-        takasurePool.burnTokens(address(0), MINT_AMOUNT);
-    }
-
-    function testTakasurePool_mustRevertIfTryToBurnMoreThanBalance() public {
-        uint256 userBalance = takasurePool.getMintedTokensByUser(user);
-        vm.prank(address(membersModule));
-        vm.expectRevert(
-            abi.encodeWithSelector(
-                TakasurePool.TakaSurePool__BurnAmountExceedsBalance.selector,
-                userBalance,
-                MINT_AMOUNT
-            )
-        );
-        takasurePool.burnTokens(user, MINT_AMOUNT);
-    }
-
-    /*//////////////////////////////////////////////////////////////
                              MINT FUNCTION
     //////////////////////////////////////////////////////////////*/
 
@@ -133,5 +82,56 @@ contract TakasurePoolTest is Test {
         uint256 userBalanceFromMappingAfter = takasurePool.getMintedTokensByUser(user);
 
         assert(userBalanceFromMappingBefore > userBalanceFromMappingAfter);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                GETTERS
+    //////////////////////////////////////////////////////////////*/
+    function testTakasurePool_membersModuleIsMinterAndBurner() public view {
+        bytes32 MINTER_ROLE = keccak256("MINTER_ROLE");
+        bytes32 BURNER_ROLE = keccak256("BURNER_ROLE");
+
+        bool isMinter = takasurePool.hasRole(MINTER_ROLE, address(membersModule));
+        bool isBurner = takasurePool.hasRole(BURNER_ROLE, address(membersModule));
+        // bool isMinter = takasurePool.hasRole(MINTER_ROLE, address(proxy));
+        // bool isBurner = takasurePool.hasRole(BURNER_ROLE, address(proxy));
+
+        assert(isMinter);
+        assert(isBurner);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                REVERTS
+    //////////////////////////////////////////////////////////////*/
+
+    function testTakasurePool_mustRevertIfTryToMintToAddressZero() public {
+        vm.prank(address(membersModule));
+        vm.expectRevert(TakasurePool.TakasurePool__NotZeroAddress.selector);
+        takasurePool.mint(address(0), MINT_AMOUNT);
+    }
+
+    function testTakasurePool_mustRevertIfTryToMintZero() public {
+        vm.prank(address(membersModule));
+        vm.expectRevert(TakasurePool.TakasurePool__MustBeMoreThanZero.selector);
+        takasurePool.mint(user, 0);
+    }
+
+    function testTakasurePool_mustRevertIfTryToBurnFromAddressZero() public {
+        vm.prank(address(membersModule));
+        vm.expectRevert(TakasurePool.TakasurePool__NotZeroAddress.selector);
+        takasurePool.burnTokens(address(0), MINT_AMOUNT);
+    }
+
+    function testTakasurePool_mustRevertIfTryToBurnMoreThanBalance() public {
+        uint256 userBalance = takasurePool.getMintedTokensByUser(user);
+        vm.prank(address(membersModule));
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                TakasurePool.TakaSurePool__BurnAmountExceedsBalance.selector,
+                userBalance,
+                MINT_AMOUNT
+            )
+        );
+        takasurePool.burnTokens(user, MINT_AMOUNT);
     }
 }
