d5a2b48572171371c12d5dc09fbec8f874356352
Maikel Ordaz <maikelordaz@gmail.com>
2024-04-25 19:02:55 -0400

    chore: update configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -41,16 +41,7 @@ module.exports = {
     solidity: {
         compilers: [
             {
-                version: "0.6.12",
-                settings: {
-                    optimizer: {
-                        enabled: true,
-                        runs: 200,
-                    },
-                },
-            },
-            {
-                version: "0.8.24",
+                version: "0.8.25",
                 settings: {
                     evmVersion: "cancun",
                     optimizer: {
