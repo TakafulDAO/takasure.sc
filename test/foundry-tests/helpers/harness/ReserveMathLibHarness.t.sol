87682fb3c1333ee60f83ae1f37f4de1c35f9ad6f
Maikel Ordaz <maikelordaz@gmail.com>
2024-05-22 10:10:18 -0400

    revise: handler to test internal functions

---
+++ test/foundry-tests/helpers/harness/ReserveMathLibHarness.t.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: GPL-3.0
+// @dev This contract is used to be able to test some internal functions
+
+pragma solidity 0.8.25;
+
+import {ReserveMathLib} from "../../../../contracts/libraries/ReserveMathLib.sol";
+
+contract ReserveMathLibHarness {
+    function exposed__updateProFormaFundReserve(
+        uint256 currentProFormaFundReserve,
+        uint256 memberNetContribution,
+        uint256 currentDynamicReserveRatio
+    ) external pure returns (uint256 exposedUpdatedProFormaFundReserve) {
+        exposedUpdatedProFormaFundReserve = ReserveMathLib._updateProFormaFundReserve(
+            currentProFormaFundReserve,
+            memberNetContribution,
+            currentDynamicReserveRatio
+        );
+    }
+
+    function exposed__calculateDynamicReserveRatioReserveShortfallMethod(
+        uint256 currentDynamicReserveRatio,
+        uint256 proFormaFundReserve,
+        uint256 fundReserve,
+        uint256 cashFlowLastPeriod
+    ) external pure returns (uint256 exposedUpdatedDynamicReserveRatio) {
+        exposedUpdatedDynamicReserveRatio = ReserveMathLib
+            ._calculateDynamicReserveRatioReserveShortfallMethod(
+                currentDynamicReserveRatio,
+                proFormaFundReserve,
+                fundReserve,
+                cashFlowLastPeriod
+            );
+    }
+
+    function exposed__calculateDaysPassed(
+        uint256 finalDayTimestamp,
+        uint256 initialDayTimestamp
+    ) external pure returns (uint256 exposedDaysPassed) {
+        exposedDaysPassed = ReserveMathLib._calculateDaysPassed(
+            finalDayTimestamp,
+            initialDayTimestamp
+        );
+    }
+
+    function exposed__calculateMonthsPassed(
+        uint256 finalMonthTimestamp,
+        uint256 initialMonthTimestamp
+    ) external pure returns (uint256 exposedMonthsPassed) {
+        exposedMonthsPassed = ReserveMathLib._calculateMonthsPassed(
+            finalMonthTimestamp,
+            initialMonthTimestamp
+        );
+    }
+}
