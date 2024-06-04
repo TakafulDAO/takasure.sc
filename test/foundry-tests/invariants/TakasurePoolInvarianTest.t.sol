a7c49b5e2c89d99f99ef46698b840071150829cf
Maikel Ordaz <maikelordaz@gmail.com>
2024-06-04 07:18:00 -0400

    Revert "chore: remove unused variable in test"
    This reverts commit af9f4b00728d7964cb191248710126b50dab81c2.

--- test/foundry-tests/invariants/TakasurePoolInvarianTest.t.sol
+++ test/foundry-tests/invariants/TakasurePoolInvarianTest.t.sol
@@ -49,7 +49,15 @@ contract TakasurePoolInvariantTest is StdInvariant, Test {
 
     /// @dev Invariant to check dynamic reserve ratio. Can not be greater than 100
     function invariant_dynamicReserveRatioCanNotBeGreaterThan100() public view {
-        (, uint256 drr, , , , , ) = takasurePool.getPoolValues();
+        (
+            uint256 proformaFundReserve,
+            uint256 drr,
+            ,
+            uint256 totalContributions,
+            uint256 totalClaimReserve,
+            uint256 totalFundReserve,
+
+        ) = takasurePool.getPoolValues();
 
         console2.log("Dynamic Reserve Ratio: ", drr);
         // console2.log("Month", takasurePool.monthReference());
