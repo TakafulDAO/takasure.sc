8773673a3a7cbb50b13171bd2030d3e2baa3d77f
Maikel Ordaz <maikelordaz@gmail.com>
2024-06-10 21:43:37 -0400

    revise: initialize bmaFundReserveShare

--- contracts/takasure/TakasurePool.sol
+++ contracts/takasure/TakasurePool.sol
@@ -103,8 +103,9 @@ contract TakasurePool is Initializable, UUPSUpgradeable, OwnableUpgradeable {
 
         reserve.initialReserveRatio = 40; // 40% Default
         reserve.dynamicReserveRatio = reserve.initialReserveRatio; // Default
-        reserve.benefitMultiplierAdjuster = 100; // Default
+        reserve.benefitMultiplierAdjuster = 100; // 100% Default
         reserve.wakalaFee = 20; // 20% of the contribution amount. Default
+        reserve.bmaFundReserveShare = 70; // 70% Default
     }
 
     /**
