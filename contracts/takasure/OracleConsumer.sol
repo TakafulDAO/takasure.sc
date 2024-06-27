06d8a8192caab03dcc1bf8eb66a8ce562ec73859
Maikel Ordaz <maikelordaz@gmail.com>
2024-06-27 15:21:07 -0400

    chore: todo comment

--- contracts/takasure/OracleConsumer.sol
+++ contracts/takasure/OracleConsumer.sol
@@ -17,7 +17,7 @@ contract OracleConsumer is FunctionsClient {
     bytes public lastResponse;
     bytes public lastError;
 
-    event Response(bytes32 indexed requestId, bytes response, bytes err);
+    event Response(bytes32 indexed requestId, bytes response, bytes err); // todo: this will be emited during the callback, check if chainlink need this exact name or can use OnResponse
 
     error OracleConsumer__UnexpectedRequestID(bytes32 requestId);
 }
