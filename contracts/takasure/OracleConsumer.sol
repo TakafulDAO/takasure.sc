8c61421850f2ae633a62a1f508f17b3ba10f8078
Maikel Ordaz <maikelordaz@gmail.com>
2024-06-27 15:09:39 -0400

    revise: import Functions contracts

--- contracts/takasure/OracleConsumer.sol
+++ contracts/takasure/OracleConsumer.sol
@@ -5,6 +5,9 @@
  * @author Maikel Ordaz
  */
 
+import {FunctionsClient} from "@chainlink/contracts/src/v0.8/functions/v1_3_0/FunctionsClient.sol";
+import {FunctionsRequest} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/libraries/FunctionsRequest.sol";
+
 pragma solidity 0.8.25;
 
-contract OracleConsumer {}
+contract OracleConsumer is FunctionsClient {}
