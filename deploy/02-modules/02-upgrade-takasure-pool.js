041d4657d2fb2f637e8a64241c83db5c9a5daffc
Maikel Ordaz <maikelordaz@gmail.com>
2024-07-15 21:34:04 -0400

    revise: new upgrade script

---
+++ deploy/02-modules/02-upgrade-takasure-pool.js
@@ -0,0 +1,53 @@
+const { network, ethers, upgrades } = require("hardhat")
+const { isDevnet, networkConfig } = require("../../utils/_networks")
+
+module.exports = async ({ deployments }) => {
+    const { log } = deployments
+    const chainId = network.config.chainId
+
+    let usdc, usdcAddress
+    let daoToken, daoTokenAddress, wakalaClaimAddress
+    let takasureProxy, takasureProxyAddress
+
+    log("02.02. Deploying TakasurePool Contract...")
+
+    if (isDevnet) {
+        usdc = await deployments.get("USDC")
+        usdcAddress = usdc.address
+    } else {
+        usdcAddress = networkConfig[chainId]["usdc"]
+    }
+
+    daoToken = await deployments.get("TSToken")
+    daoTokenAddress = daoToken.address
+    wakalaClaimAddress = networkConfig[chainId]["wakalaClaimAddress"]
+    daoOperator = networkConfig[chainId]["daoOperator"]
+    takasureProxy = await deployments.get("TakasurePool")
+    takasureProxyAddress = takasureProxy.address
+
+    const initArgs = [usdcAddress, daoTokenAddress, wakalaClaimAddress, daoOperator]
+
+    const TakasurePoolUpgrade = await ethers.getContractFactory("TakasurePool")
+    const takasurePoolUpgrade = await upgrades.upgradeProxy(
+        takasureProxyAddress,
+        TakasurePoolUpgrade,
+    )
+
+    const artifact = await deployments.getArtifact("TakasurePool")
+
+    log("02.02. Writing TakasurePool Contract Deployment Data...")
+
+    deployments.save("TakasurePool", {
+        abi: artifact.abi,
+        address: takasureProxyAddress,
+        bytecode: artifact.bytecode,
+        deployedBytecode: artifact.deployedBytecode,
+    })
+
+    console.log("02.02. TakasurePool Data stored in the deployments folder")
+
+    log("02.02. TakasurePool Contract Upgraded!")
+    log("=====================================================================================")
+}
+
+module.exports.tags = ["upgrade"]
