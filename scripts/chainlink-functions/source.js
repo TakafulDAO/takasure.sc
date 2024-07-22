ba21a0665551205c4f00b9d561ab6880e36ca509
Maikel Ordaz <maikelordaz@gmail.com>
2024-07-21 22:22:49 -0400

    revise: fix request script

--- scripts/chainlink-functions/source.js
+++ scripts/chainlink-functions/source.js
@@ -1,11 +1,8 @@
-// Arguments can be provided when a request is initated on-chain and used in the request source code as shown below
 const userAddress = args[0]
 
 // make HTTP request
 const url = `http://34.228.189.184:5559/api/v1/user/bm`
 console.log(`HTTP GET Request to ${url}?fsyms=${userAddress}`)
-
-// construct the HTTP Request object. See: https://github.com/smartcontractkit/functions-hardhat-starter-kit#javascript-code
 
 const bmRequest = Functions.makeHttpRequest({
     url: url,
@@ -21,8 +18,6 @@ console.log("Waiting for response...")
 // Execute the API request (Promise)
 const bmResponse = await bmRequest
 if (bmResponse.error) {
-    // console.error(bmResponse.error)
-    // throw Error("Request failed")
     return Functions.encodeUint256(0)
 }
 
@@ -32,7 +27,6 @@ if (data.Response === "Error") {
     throw Error(`Functional error. Read message: ${data.Message}`)
 }
 
-// extract the price
 const bm = data["BenefitMultiplier"]
 console.log(`${userAddress} bm is: ${bm}`)
 
