3e973c88b75675100c71332badf2f51834e224fc
Maikel Ordaz <maikelordaz@gmail.com>
2024-07-21 20:54:03 -0400

    revise: fetch bm script

--- scripts/chainlink-functions/source.js
+++ scripts/chainlink-functions/source.js
@@ -1,29 +1,39 @@
 // Arguments can be provided when a request is initated on-chain and used in the request source code as shown below
-const userAddress = args[0];
+const userAddress = args[0]
 
-// make HTTP request /api/v1/user/bm [get]
-const url = `https://takasure.io/api/v1/user/bm`;
-console.log(`HTTP GET Request to ${url}?WalletAddr=${userAddress}`);
+// make HTTP request
+const url = `http://34.228.189.184:5559/api/v1/user/bm`
+console.log(`HTTP GET Request to ${url}?fsyms=${userAddress}`)
+
+// construct the HTTP Request object. See: https://github.com/smartcontractkit/functions-hardhat-starter-kit#javascript-code
 
 const bmRequest = Functions.makeHttpRequest({
-  url: url,
-  method: 'GET',
-  params: {
-    WalletAddr: userAddress,
-  },
-  });
+    url: url,
+    method: "GET",
+    params: {
+        WalletAddr: userAddress,
+    },
+})
+
+console.log("Request sent to BM API")
+console.log("Waiting for response...")
 
 // Execute the API request (Promise)
-const bmResponse = await bmRequest;
+const bmResponse = await bmRequest
 if (bmResponse.error) {
-  console.error(bmResponse.error);
-  throw Error("Request failed");
+    // console.error(bmResponse.error)
+    // throw Error("Request failed")
+    return Functions.encodeUint256(0)
 }
 
-const data = bmResponse["data"];
+const data = bmResponse["data"]
 if (data.Response === "Error") {
-  console.error(data.Message);
-  throw Error(`Functional error. Read message: ${data.Message}`);
+    console.error(data.Message)
+    throw Error(`Functional error. Read message: ${data.Message}`)
 }
 
-// get BM
+// extract the price
+const bm = data["BenefitMultiplier"]
+console.log(`${userAddress} bm is: ${bm}`)
+
+return Functions.encodeUint256(bm)
