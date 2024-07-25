c2d6cef1eb10d7d54ba77fba9bea79deb4b4dfdf
Maikel Ordaz <maikelordaz@gmail.com>
2024-07-25 15:12:23 -0400

    revise: change access control

--- contracts/takasure/oracle/BmFetcher.sol
+++ contracts/takasure/oracle/BmFetcher.sol
@@ -1,20 +1,22 @@
 //SPDX-License-Identifier: GPL-3.0
 
 /**
- * @title FunctionsConsumer
+ * @title BmFetcher
  * @author Maikel Ordaz
+ * @notice This contract is used to fetch the benefit multiplier to be used in the Life Dao protocol
  */
 
-// todo: natspec
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 import {FunctionsClient} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/FunctionsClient.sol";
 
 import {FunctionsRequest} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/libraries/FunctionsRequest.sol";
 
 pragma solidity 0.8.25;
 
-contract BmFetcher is Ownable, FunctionsClient {
+contract BmFetcher is AccessControl, FunctionsClient {
     using FunctionsRequest for FunctionsRequest.Request;
+
+    bytes32 public constant BM_REQUESTER_ROLE = keccak256("BM_REQUESTER_ROLE");
 
     bytes32 private donId;
     uint32 private gasLimit;
@@ -29,13 +31,23 @@ contract BmFetcher is Ownable, FunctionsClient {
 
     error OracleConsumer__UnexpectedRequestID(bytes32 requestId);
 
-    /// @custom:oz-upgrades-unsafe-allow constructor
+    /**
+     * @custom:oz-upgrades-unsafe-allow constructor
+     * @param router The address of the Chainlink node
+     * @param _donId The data provider ID
+     * @param _gasLimit The gas limit for the request
+     * @param _subscriptionId The subscription ID
+     * @param requester The address allowed to request the benefit multiplier. Will be the takasure contract
+     */
     constructor(
         address router,
         bytes32 _donId,
         uint32 _gasLimit,
-        uint64 _subscriptionId
-    ) Ownable(msg.sender) FunctionsClient(router) {
+        uint64 _subscriptionId,
+        address requester
+    ) FunctionsClient(router) {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(BM_REQUESTER_ROLE, requester);
         donId = _donId;
         gasLimit = _gasLimit;
         subscriptionId = _subscriptionId;
@@ -43,9 +55,11 @@ contract BmFetcher is Ownable, FunctionsClient {
 
     /**
      * @notice Send a simple request
-     * @param args List of arguments accessible from within the source code
+     * @param args The user address to get the BM
      */
-    function sendRequest(string[] memory args) external onlyOwner returns (bytes32 requestId) {
+    function sendRequest(
+        string[] memory args
+    ) external onlyRole(BM_REQUESTER_ROLE) returns (bytes32 requestId) {
         FunctionsRequest.Request memory req;
         req.initializeRequestForInlineJavaScript(bmSourceRequestCode);
 
@@ -55,19 +69,21 @@ contract BmFetcher is Ownable, FunctionsClient {
     }
 
     /// @notice the next set of functions are used to set the values of the contract
-    function setDonId(bytes32 _donId) external onlyOwner {
+    function setDonId(bytes32 _donId) external onlyRole(DEFAULT_ADMIN_ROLE) {
         donId = _donId;
     }
 
-    function setGasLimit(uint32 _gasLimit) external onlyOwner {
+    function setGasLimit(uint32 _gasLimit) external onlyRole(DEFAULT_ADMIN_ROLE) {
         gasLimit = _gasLimit;
     }
 
-    function setSubscriptionId(uint64 _subscriptionId) external onlyOwner {
+    function setSubscriptionId(uint64 _subscriptionId) external onlyRole(DEFAULT_ADMIN_ROLE) {
         subscriptionId = _subscriptionId;
     }
 
-    function setBMSourceRequestCode(string calldata _bmSourceRequestCode) external onlyOwner {
+    function setBMSourceRequestCode(
+        string calldata _bmSourceRequestCode
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
         bmSourceRequestCode = _bmSourceRequestCode;
     }
 
