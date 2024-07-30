adf2b5980cd786a6542d2098bff9127e7e47fa2c
Maikel Ordaz <maikelordaz@gmail.com>
2024-07-29 23:52:15 -0400

    feat: new events for remaining storage variable change (TS-265) (#20)
    * revise: new events
    
    * revise: migrate events to library
    
    * reviseÑ migrate errors to library
    
    * chore: fix tests
    
    * revise: add memberId for better subgraph handling
    
    * chore: fix tests
    
    * chore: rename files

---
+++ contracts/libraries/TakasureErrors.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0
+
+/**
+ * @title Errors
+ * @author  Maikel Ordaz
+ * @notice  This library is used to store the errors of the Takasure protocol
+ */
+pragma solidity 0.8.25;
+
+library TakasureErrors {
+    error TakasurePool__MemberAlreadyExists();
+    error TakasurePool__ZeroAddress();
+    error TakasurePool__ContributionBelowMinimumThreshold();
+    error TakasurePool__ContributionTransferFailed();
+    error TakasurePool__FeeTransferFailed();
+    error TakasurePool__MintFailed();
+    error TakasurePool__WrongServiceFee();
+    error TakasurePool__MemberAlreadyKYCed();
+    error TakasurePool__WrongMemberState();
+    error TakasurePool__InvalidDate();
+}
