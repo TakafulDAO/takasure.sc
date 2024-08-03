60cbcb707f2c2a154b9749f5a1e9cafbe4dfef90
Maikel Ordaz <maikelordaz@gmail.com>
2024-08-02 23:45:08 -0400

    chore: remove unused files

--- utils/units.js
+++ utils/units.js
@@ -1,13 +1,5 @@
 const { parseUnits, formatUnits } = require("ethers")
 
-const now = Math.floor(Date.now() / 1000)
-const minute = 60
-const hour = 3600
-const day = 24 * hour
-const week = day * 7
-const month = (365 / 12) * day
-const year = month * 12
-
 function erc20Units(amount) {
     return parseUnits(amount, 18)
 }
@@ -25,13 +17,6 @@ function usdtUnitsFormat(amount) {
 }
 
 module.exports = {
-    now,
-    minute,
-    hour,
-    day,
-    week,
-    month,
-    year,
     erc20Units,
     erc20UnitsFormat,
     usdtUnits,
