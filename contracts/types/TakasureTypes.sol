418faf24e21acf7f5bd11dfdb9bc796e68e486c4
Maikel Ordaz <maikelordaz@gmail.com>
2024-08-03 22:40:06 -0400

    revise: change initial service fee value

--- contracts/types/TakasureTypes.sol
+++ contracts/types/TakasureTypes.sol
@@ -35,7 +35,7 @@ struct Reserve {
     uint256 proFormaFundReserve; // Used to update the dynamic reserve ratio
     uint256 proFormaClaimReserve;
     uint256 lossRatio; // Default 0
-    uint8 serviceFee; // Default 20%, max 100%
+    uint8 serviceFee; // Default 22%, max 100%
     uint8 bmaFundReserveShare; // Default 70%
     bool isOptimizerEnabled; // Default false
 }
