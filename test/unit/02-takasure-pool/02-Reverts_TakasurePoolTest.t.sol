76b9f32c5763821179441b41fbb9a2db32a647a4
Maikel Ordaz <maikelordaz@gmail.com>
2024-08-29 19:01:55 -0400

    chore: fix fourth case

--- test/unit/02-takasure-pool/02-Reverts_TakasurePoolTest.t.sol
+++ test/unit/02-takasure-pool/02-Reverts_TakasurePoolTest.t.sol
@@ -27,6 +27,7 @@ contract Reverts_TakasurePoolTest is StdCheats, Test, SimulateDonResponse {
     address public bob = makeAddr("bob");
     address public charlie = makeAddr("charlie");
     address public david = makeAddr("david");
+    address public erin = makeAddr("erin");
     uint256 public constant USDC_INITIAL_AMOUNT = 150e6; // 100 USDC
     uint256 public constant CONTRIBUTION_AMOUNT = 25e6; // 25 USDC
     uint256 public constant BENEFIT_MULTIPLIER = 0;
@@ -51,17 +52,17 @@ contract Reverts_TakasurePoolTest is StdCheats, Test, SimulateDonResponse {
         deal(address(usdc), bob, USDC_INITIAL_AMOUNT);
         deal(address(usdc), charlie, USDC_INITIAL_AMOUNT);
         deal(address(usdc), david, USDC_INITIAL_AMOUNT);
+        deal(address(usdc), erin, USDC_INITIAL_AMOUNT);
 
         vm.prank(alice);
         usdc.approve(address(takasurePool), USDC_INITIAL_AMOUNT);
-
         vm.prank(bob);
         usdc.approve(address(takasurePool), USDC_INITIAL_AMOUNT);
-
         vm.prank(charlie);
         usdc.approve(address(takasurePool), USDC_INITIAL_AMOUNT);
-
         vm.prank(david);
+        usdc.approve(address(takasurePool), USDC_INITIAL_AMOUNT);
+        vm.prank(erin);
         usdc.approve(address(takasurePool), USDC_INITIAL_AMOUNT);
 
         vm.prank(admin);
@@ -312,7 +313,7 @@ contract Reverts_TakasurePoolTest is StdCheats, Test, SimulateDonResponse {
         takasurePool.joinPool(CONTRIBUTION_AMOUNT, 5 * YEAR);
         vm.stopPrank();
 
-        // Fourth check david join -> 14 days passes -> david refund -> david join -> kyc david -> david join again must revert
+        // Fourth check david join -> 14 days passes -> david refund -> kyc david -> david join -> david join again must revert
         vm.prank(david);
         takasurePool.joinPool(CONTRIBUTION_AMOUNT, 5 * YEAR);
 
@@ -321,14 +322,13 @@ contract Reverts_TakasurePoolTest is StdCheats, Test, SimulateDonResponse {
 
         takasurePool.refund(david);
 
-        vm.prank(david);
-        takasurePool.joinPool(CONTRIBUTION_AMOUNT, 5 * YEAR);
-
         vm.prank(admin);
         takasurePool.setKYCStatus(david);
 
-        vm.prank(david);
+        vm.startPrank(david);
+        takasurePool.joinPool(CONTRIBUTION_AMOUNT, 5 * YEAR);
         vm.expectRevert(TakasureErrors.TakasurePool__WrongMemberState.selector);
         takasurePool.joinPool(CONTRIBUTION_AMOUNT, 5 * YEAR);
+        vm.stopPrank();
     }
 }
