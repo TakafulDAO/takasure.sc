622a30880cad7980eb3882c0d3e0c266a0b40a51
Maikel Ordaz <maikelordaz@gmail.com>
2024-09-12 21:50:33 -0400

    revise: update token interface

--- contracts/interfaces/ITSToken.sol
+++ contracts/interfaces/ITSToken.sol
@@ -3,12 +3,6 @@
 pragma solidity 0.8.25;
 
 interface ITSToken {
-    /// @notice Mint Takasure powered tokens
-    /// @param to The address to mint tokens to
-    /// @param amountToMint The amount of tokens to mint
+    function balanceOf(address account) external view returns (uint256);
     function mint(address to, uint256 amountToMint) external returns (bool);
-
-    /// @notice Burn Takasure powered tokens
-    /// @param amountToBurn The amount of tokens to burn
-    function burn(uint256 amountToBurn) external;
 }
