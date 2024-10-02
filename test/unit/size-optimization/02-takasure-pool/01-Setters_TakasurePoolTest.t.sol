f4c4232d2dde586df1f8ec938b4df116a70fb41e
Maikel Ordaz <maikelordaz@gmail.com>
2024-10-02 18:43:13 -0400

    revise: migrate setter tests

---
+++ test/unit/size-optimization/02-takasure-pool/01-Setters_TakasurePoolTest.t.sol
@@ -0,0 +1,154 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.25;
+
+import {Test, console} from "forge-std/Test.sol";
+import {DeployTakasureReserve} from "test/utils/TestDeployTakasureReserve.s.sol";
+import {DeployConsumerMocks} from "test/utils/DeployConsumerMocks.s.sol";
+import {HelperConfig} from "deploy/HelperConfig.s.sol";
+import {TakasureReserve} from "contracts/takasure/core/TakasureReserve.sol";
+import {JoinModule} from "contracts/takasure/modules/JoinModule.sol";
+import {MembersModule} from "contracts/takasure/modules/MembersModule.sol";
+import {BenefitMultiplierConsumerMock} from "test/mocks/BenefitMultiplierConsumerMock.sol";
+import {StdCheats} from "forge-std/StdCheats.sol";
+import {IUSDC} from "test/mocks/IUSDCmock.sol";
+import {TakasureEvents} from "contracts/libraries/TakasureEvents.sol";
+
+contract Size_Setters_TakasureReserveTest is StdCheats, Test {
+    DeployTakasureReserve deployer;
+    DeployConsumerMocks mockDeployer;
+    TakasureReserve takasureReserve;
+    HelperConfig helperConfig;
+    BenefitMultiplierConsumerMock bmConsumerMock;
+    JoinModule joinModule;
+    MembersModule membersModule;
+    address takasureReserveProxy;
+    address contributionTokenAddress;
+    address admin;
+    address joinModuleAddress;
+    address membersModuleAddress;
+    IUSDC usdc;
+    address public alice = makeAddr("alice");
+    uint256 public constant USDC_INITIAL_AMOUNT = 100e6; // 100 USDC
+    uint256 public constant CONTRIBUTION_AMOUNT = 25e6; // 25 USDC
+    uint256 public constant BENEFIT_MULTIPLIER = 0;
+    uint256 public constant YEAR = 365 days;
+
+    function setUp() public {
+        deployer = new DeployTakasureReserve();
+        (
+            takasureReserveProxy,
+            joinModuleAddress,
+            membersModuleAddress,
+            contributionTokenAddress,
+            helperConfig
+        ) = deployer.run();
+
+        joinModule = JoinModule(joinModuleAddress);
+        membersModule = MembersModule(membersModuleAddress);
+
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfigByChainId(block.chainid);
+
+        admin = config.daoMultisig;
+
+        deployer.assignAddresses(
+            config.takadaoOperator,
+            takasureReserveProxy,
+            joinModuleAddress,
+            membersModuleAddress
+        );
+
+        mockDeployer = new DeployConsumerMocks();
+        bmConsumerMock = mockDeployer.run();
+
+        takasureReserve = TakasureReserve(takasureReserveProxy);
+        usdc = IUSDC(contributionTokenAddress);
+
+        // For easier testing there is a minimal USDC mock contract without restrictions
+        deal(address(usdc), alice, USDC_INITIAL_AMOUNT);
+
+        vm.startPrank(alice);
+        usdc.approve(address(takasureReserve), USDC_INITIAL_AMOUNT);
+        vm.stopPrank();
+
+        vm.prank(admin);
+        takasureReserve.setNewBenefitMultiplierConsumerAddress(address(bmConsumerMock));
+
+        vm.prank(msg.sender);
+        bmConsumerMock.setNewRequester(address(takasureReserve));
+    }
+
+    /// @dev Test the owner can set a new service fee
+    function testTakasureReserve_setNewServiceFeeToNewValue() public {
+        assert(2 + 2 == 4);
+        uint8 newServiceFee = 35;
+
+        vm.prank(admin);
+        vm.expectEmit(true, false, false, false, address(takasureReserve));
+        emit TakasureEvents.OnServiceFeeChanged(newServiceFee);
+        takasureReserve.setNewServiceFee(newServiceFee);
+
+        uint8 serviceFee = takasureReserve.getReserveValues().serviceFee;
+
+        assertEq(newServiceFee, serviceFee);
+    }
+
+    /// @dev Test the owner can set a new minimum threshold
+    function testTakasureReserve_setNewMinimumThreshold() public {
+        uint256 newMinimumThreshold = 50e6;
+
+        vm.prank(admin);
+        vm.expectEmit(true, false, false, false, address(takasureReserve));
+        emit TakasureEvents.OnNewMinimumThreshold(newMinimumThreshold);
+        takasureReserve.setNewMinimumThreshold(newMinimumThreshold);
+
+        assertEq(newMinimumThreshold, takasureReserve.getReserveValues().minimumThreshold);
+    }
+
+    /// @dev Test the owner can set a new minimum threshold
+    function testTakasureReserve_setNewMaximumThreshold() public {
+        uint256 newMaximumThreshold = 50e6;
+
+        vm.prank(admin);
+        vm.expectEmit(true, false, false, false, address(takasureReserve));
+        emit TakasureEvents.OnNewMaximumThreshold(newMaximumThreshold);
+        takasureReserve.setNewMaximumThreshold(newMaximumThreshold);
+
+        assertEq(newMaximumThreshold, takasureReserve.getReserveValues().maximumThreshold);
+    }
+
+    /// @dev Test the owner can set a new contribution token
+    function testTakasureReserve_setNewContributionToken() public {
+        vm.prank(admin);
+        takasureReserve.setNewContributionToken(alice);
+
+        assertEq(alice, takasureReserve.getReserveValues().contributionToken);
+    }
+
+    /// @dev Test the owner can set a new service claim address
+    function testTakasureReserve_cansetNewServiceClaimAddress() public {
+        vm.prank(admin);
+        takasureReserve.setNewFeeClaimAddress(alice);
+
+        assertEq(alice, takasureReserve.feeClaimAddress());
+    }
+
+    /// @dev Test the owner can set custom duration
+    function testTakasureReserve_setAllowCustomDuration() public {
+        vm.prank(admin);
+        takasureReserve.setAllowCustomDuration(true);
+
+        assertEq(true, takasureReserve.getReserveValues().allowCustomDuration);
+    }
+
+    // function testTakasureReserve_setKYCstatus() public {
+    //     assert(!takasureReserve.getMemberFromAddress(alice).isKYCVerified);
+
+    //     vm.prank(admin);
+    //     vm.expectEmit(true, false, false, false, address(joinModule));
+    //     emit TakasureEvents.OnMemberKycVerified(1, alice);
+    //     joinModule.setKYCStatus(alice);
+
+    //     assert(takasureReserve.getMemberFromAddress(alice).isKYCVerified);
+    // }
+}
