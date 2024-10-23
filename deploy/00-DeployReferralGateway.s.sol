779864a4dd67f1a1ed3a14f36c75bdc457b82aa4
Maikel Ordaz <maikelordaz@gmail.com>
2024-10-23 10:08:34 -0400

    chore: new deployments

--- deploy/00-DeployReferralGateway.s.sol
+++ deploy/00-DeployReferralGateway.s.sol
@@ -2,16 +2,25 @@
 
 pragma solidity 0.8.25;
 
-import {Script, console2} from "forge-std/Script.sol";
+import {Script, console2, stdJson} from "forge-std/Script.sol";
 import {BenefitMultiplierConsumer} from "contracts/takasure/oracle/BenefitMultiplierConsumer.sol";
 import {ReferralGateway} from "contracts/referrals/ReferralGateway.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 import {Upgrades} from "openzeppelin-foundry-upgrades/src/Upgrades.sol";
 
 contract DeployReferralGateway is Script {
+    using stdJson for string;
+
     function run() external returns (address proxy) {
         HelperConfig helperConfig = new HelperConfig();
         HelperConfig.NetworkConfig memory config = helperConfig.getConfigByChainId(block.chainid);
+
+        string memory root = vm.projectRoot();
+        string memory scriptPath = string.concat(
+            root,
+            "/scripts/chainlink-functions/bmFetchCode.js"
+        );
+        string memory bmFetchScript = vm.readFile(scriptPath);
 
         vm.startBroadcast();
 
@@ -21,6 +30,9 @@ contract DeployReferralGateway is Script {
             config.gasLimit,
             config.subscriptionId
         );
+
+        // Add new source code to BenefitMultiplierConsumer
+        bmConsumer.setBMSourceRequestCode(bmFetchScript);
 
         // Deploy TakasurePool
         proxy = Upgrades.deployUUPSProxy(
