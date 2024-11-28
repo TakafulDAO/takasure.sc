908a6c7e5dd448d50970f09177c3cec2aa9f7840
Maikel Ordaz <maikelordaz@gmail.com>
2024-11-28 06:09:35 -0400

    chore: temporary disable script

--- scripts/contract-interactions/referralGateway/ChangeAdmin.s.sol
+++ scripts/contract-interactions/referralGateway/ChangeAdmin.s.sol
@@ -1,23 +1,23 @@
-// SPDX-License-Identifier: GPL-3.0
+// // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity 0.8.28;
+// pragma solidity 0.8.28;
 
-import {Script, console2, GetContractAddress} from "scripts/utils/GetContractAddress.s.sol";
-import {ReferralGateway} from "contracts/referrals/ReferralGateway.sol";
+// import {Script, console2, GetContractAddress} from "scripts/utils/GetContractAddress.s.sol";
+// import {ReferralGateway} from "contracts/referrals/ReferralGateway.sol";
 
-contract ChangeAdmin is Script, GetContractAddress {
-    function run() public {
-        address referralGatewayAddress = _getContractAddress(block.chainid, "ReferralGateway");
+// contract ChangeAdmin is Script, GetContractAddress {
+//     function run() public {
+//         address referralGatewayAddress = _getContractAddress(block.chainid, "ReferralGateway");
 
-        ReferralGateway referralGateway = ReferralGateway(referralGatewayAddress);
+//         ReferralGateway referralGateway = ReferralGateway(referralGatewayAddress);
 
-        address safeMultiSig = vm.envAddress("MULTISIG_ADDRESS"); // ! CHANGE THIS
+//         address safeMultiSig = vm.envAddress("MULTISIG_ADDRESS"); // ! CHANGE THIS
 
-        vm.startBroadcast();
+//         vm.startBroadcast();
 
-        referralGateway.grantRole({role: 0x00, account: safeMultiSig});
-        referralGateway.renounceRole({role: 0x00, callerConfirmation: msg.sender});
+//         referralGateway.grantRole({role: 0x00, account: safeMultiSig});
+//         referralGateway.renounceRole({role: 0x00, callerConfirmation: msg.sender});
 
-        vm.stopBroadcast();
-    }
-}
+//         vm.stopBroadcast();
+//     }
+// }
--- scripts/contract-interactions/referralGateway/ChangeOperator.s.sol
+++ scripts/contract-interactions/referralGateway/ChangeOperator.s.sol
@@ -1,33 +1,33 @@
-// SPDX-License-Identifier: GPL-3.0
+// // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity 0.8.28;
+// pragma solidity 0.8.28;
 
-import {Script, console2, GetContractAddress} from "scripts/utils/GetContractAddress.s.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {ReferralGateway} from "contracts/referrals/ReferralGateway.sol";
-import {HelperConfig} from "deploy/HelperConfig.s.sol";
+// import {Script, console2, GetContractAddress} from "scripts/utils/GetContractAddress.s.sol";
+// import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// import {ReferralGateway} from "contracts/referrals/ReferralGateway.sol";
+// import {HelperConfig} from "deploy/HelperConfig.s.sol";
 
-contract ChangeOperator is Script, GetContractAddress {
-    IERC20 public usdc;
-    HelperConfig public helperConfig;
+// contract ChangeOperator is Script, GetContractAddress {
+//     IERC20 public usdc;
+//     HelperConfig public helperConfig;
 
-    function run() public {
-        helperConfig = new HelperConfig();
-        HelperConfig.NetworkConfig memory config = helperConfig.getConfigByChainId(block.chainid);
+//     function run() public {
+//         helperConfig = new HelperConfig();
+//         HelperConfig.NetworkConfig memory config = helperConfig.getConfigByChainId(block.chainid);
 
-        address referralGatewayAddress = _getContractAddress(block.chainid, "ReferralGateway");
+//         address referralGatewayAddress = _getContractAddress(block.chainid, "ReferralGateway");
 
-        ReferralGateway referralGateway = ReferralGateway(referralGatewayAddress);
-        usdc = IERC20(config.contributionToken);
+//         ReferralGateway referralGateway = ReferralGateway(referralGatewayAddress);
+//         usdc = IERC20(config.contributionToken);
 
-        address safeMultiSig = vm.envAddress("MULTISIG_ADDRESS"); // ! CHANGE THIS
+//         address safeMultiSig = vm.envAddress("MULTISIG_ADDRESS"); // ! CHANGE THIS
 
-        vm.startBroadcast();
+//         vm.startBroadcast();
 
-        usdc.approve(referralGatewayAddress, usdc.balanceOf(msg.sender));
+//         usdc.approve(referralGatewayAddress, usdc.balanceOf(msg.sender));
 
-        referralGateway.setNewOperator({newOperator: safeMultiSig});
+//         referralGateway.setNewOperator({newOperator: safeMultiSig});
 
-        vm.stopBroadcast();
-    }
-}
+//         vm.stopBroadcast();
+//     }
+// }
