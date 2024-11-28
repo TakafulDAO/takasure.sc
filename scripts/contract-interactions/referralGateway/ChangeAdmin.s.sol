c6f8eedb58e0d4f7fc58aab343f388312ea1f2ba
Maikel Ordaz <maikelordaz@gmail.com>
2024-11-27 22:40:07 -0400

    revise: role scripts

---
+++ scripts/contract-interactions/referralGateway/ChangeAdmin.s.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.28;
+
+import {Script, console2, GetContractAddress} from "scripts/utils/GetContractAddress.s.sol";
+import {ReferralGateway} from "contracts/referrals/ReferralGateway.sol";
+
+contract ChangeAdmin is Script, GetContractAddress {
+    address public safeMultiSig = 0x0259F914D29006510177B620e828a3eD93D762cf;
+
+    function run() public {
+        address referralGatewayAddress = _getContractAddress(block.chainid, "ReferralGateway");
+
+        ReferralGateway referralGateway = ReferralGateway(referralGatewayAddress);
+
+        vm.startBroadcast();
+
+        referralGateway.grantRole({role: 0x00, account: safeMultiSig});
+        referralGateway.renounceRole({role: 0x00, callerConfirmation: msg.sender});
+
+        vm.stopBroadcast();
+    }
+}
