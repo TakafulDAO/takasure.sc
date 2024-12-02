5a947e06df49074b4317f6ca7b3a69a81b651f51
Maikel Ordaz <maikelordaz@gmail.com>
2024-12-01 20:52:23 -0400

    revise: fix upgrade script

--- deploy/01-UpgradeReferralGateway.s.sol
+++ deploy/01-UpgradeReferralGateway.s.sol
@@ -5,6 +5,7 @@ pragma solidity 0.8.28;
 import {Script, console2, stdJson, GetContractAddress} from "scripts/utils/GetContractAddress.s.sol";
 import {ReferralGateway} from "contracts/referrals/ReferralGateway.sol";
 import {Upgrades} from "openzeppelin-foundry-upgrades/src/Upgrades.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract UpgradeReferralGateway is Script, GetContractAddress {
     function run() external returns (address) {
@@ -12,10 +13,17 @@ contract UpgradeReferralGateway is Script, GetContractAddress {
         address oldImplementation = Upgrades.getImplementationAddress(referralGatewayAddress);
         console2.log("Old Referral Gateway implementation address: ", oldImplementation);
 
+        HelperConfig helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfigByChainId(block.chainid);
+
         vm.startBroadcast();
 
         // Upgrade ReferralGateway
-        Upgrades.upgradeProxy(referralGatewayAddress, "ReferralGateway.sol", "");
+        Upgrades.upgradeProxy(
+            referralGatewayAddress,
+            "ReferralGateway.sol",
+            abi.encodeCall(ReferralGateway.initializeNewVersion, (config.takadaoOperator, 2)) // Todo: Change this address, just for testing purposes
+        );
 
         vm.stopBroadcast();
 
