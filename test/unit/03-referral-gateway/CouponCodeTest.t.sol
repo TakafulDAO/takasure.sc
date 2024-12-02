a6343ecd4db75928b68a069ba1ff792278aa0f82
Maikel Ordaz <maikelordaz@gmail.com>
2024-12-02 14:06:19 -0400

    chore: cleaning test file

--- test/unit/03-referral-gateway/CouponCodeTest.t.sol
+++ test/unit/03-referral-gateway/CouponCodeTest.t.sol
@@ -5,56 +5,25 @@ pragma solidity 0.8.28;
 import {Test, console2} from "forge-std/Test.sol";
 import {TestDeployTakasure} from "test/utils/TestDeployTakasure.s.sol";
 import {ReferralGateway} from "contracts/referrals/ReferralGateway.sol";
-import {TakasurePool} from "contracts/takasure/TakasurePool.sol";
 import {BenefitMultiplierConsumerMock} from "test/mocks/BenefitMultiplierConsumerMock.sol";
 import {HelperConfig} from "deploy/HelperConfig.s.sol";
 import {IUSDC} from "test/mocks/IUSDCmock.sol";
-import {SimulateDonResponse} from "test/utils/SimulateDonResponse.sol";
-import {UnsafeUpgrades} from "openzeppelin-foundry-upgrades/src/Upgrades.sol";
 
-contract CouponCodeTest is Test, SimulateDonResponse {
+contract CouponCodeTest is Test {
     TestDeployTakasure deployer;
     ReferralGateway referralGateway;
-    TakasurePool takasurePool;
     BenefitMultiplierConsumerMock bmConsumerMock;
     HelperConfig helperConfig;
     IUSDC usdc;
     address usdcAddress;
     address proxy;
-    address daoProxy;
-    address takadao;
-    address daoAdmin;
-    address KYCProvider;
-    address referral = makeAddr("referral");
-    address member = makeAddr("member");
-    address notMember = makeAddr("notMember");
     address child = makeAddr("child");
     address couponPool = makeAddr("couponPool");
     address couponRedeemer = makeAddr("couponRedeemer");
     string tDaoName = "TheLifeDao";
     uint256 public constant USDC_INITIAL_AMOUNT = 100e6; // 100 USDC
     uint256 public constant CONTRIBUTION_AMOUNT = 25e6; // 25 USDC
-    uint256 public constant LAYER_ONE_REWARD_RATIO = 4; // Layer one reward ratio 4%
-    uint256 public constant LAYER_TWO_REWARD_RATIO = 1; // Layer two reward ratio 1%
-    uint256 public constant LAYER_THREE_REWARD_RATIO = 35; // Layer three reward ratio 0.35%
-    uint256 public constant LAYER_FOUR_REWARD_RATIO = 175; // Layer four reward ratio 0.175%
-    uint8 public constant SERVICE_FEE_RATIO = 27;
     uint256 public constant CONTRIBUTION_PREJOIN_DISCOUNT_RATIO = 10; // 10% of contribution deducted from fee
-    uint256 public constant REFERRAL_DISCOUNT_RATIO = 5; // 5% of contribution deducted from contribution
-    uint256 public constant REFERRAL_RESERVE = 5; // 5% of contribution TO Referral Reserve
-    uint256 public constant REPOOL_FEE_RATIO = 2; // 2% of contribution deducted from fee
-
-    bytes32 public constant REFERRAL = keccak256("REFERRAL");
-    bytes32 private constant OPERATOR = keccak256("OPERATOR");
-
-    struct PrepaidMember {
-        string tDAOName;
-        address member;
-        uint256 contributionBeforeFee;
-        uint256 contributionAfterFee;
-        uint256 finalFee; // Fee after all the discounts and rewards
-        uint256 discount;
-    }
 
     event OnCouponRedeemed(
         address indexed member,
@@ -66,51 +35,23 @@ contract CouponCodeTest is Test, SimulateDonResponse {
         // Deployer
         deployer = new TestDeployTakasure();
         // Deploy contracts
-        (, bmConsumerMock, daoProxy, proxy, usdcAddress, KYCProvider, helperConfig) = deployer
-            .run();
+        (, bmConsumerMock, , proxy, usdcAddress, , helperConfig) = deployer.run();
 
         // Get config values
         HelperConfig.NetworkConfig memory config = helperConfig.getConfigByChainId(block.chainid);
-        takadao = config.takadaoOperator;
-        daoAdmin = config.daoMultisig;
 
         // Assign implementations
         referralGateway = ReferralGateway(address(proxy));
-        takasurePool = TakasurePool(address(daoProxy));
         usdc = IUSDC(usdcAddress);
 
-        // Config mocks
-        vm.startPrank(daoAdmin);
-        takasurePool.setNewContributionToken(address(usdc));
-        takasurePool.setNewBenefitMultiplierConsumer(address(bmConsumerMock));
-
-        takasurePool.setNewReferralGateway(address(referralGateway));
-        vm.stopPrank();
-        vm.prank(msg.sender);
-        bmConsumerMock.setNewRequester(address(takasurePool));
-
         // Give and approve USDC
-        deal(address(usdc), referral, USDC_INITIAL_AMOUNT);
         deal(address(usdc), child, USDC_INITIAL_AMOUNT);
-        deal(address(usdc), member, USDC_INITIAL_AMOUNT);
-        vm.prank(referral);
-        usdc.approve(address(referralGateway), USDC_INITIAL_AMOUNT);
         vm.prank(child);
         usdc.approve(address(referralGateway), USDC_INITIAL_AMOUNT);
-        vm.prank(member);
-        usdc.approve(address(takasurePool), USDC_INITIAL_AMOUNT);
-
-        // Join the dao
-        vm.prank(daoAdmin);
-        takasurePool.setKYCStatus(member);
-        // We simulate a request before the KYC
-        _successResponse(address(bmConsumerMock));
-        vm.prank(member);
-        takasurePool.joinPool(CONTRIBUTION_AMOUNT, 5);
 
         ReferralGateway newImplementation = new ReferralGateway();
 
-        vm.prank(takadao);
+        vm.prank(config.takadaoOperator);
         referralGateway.upgradeToAndCall(
             address(newImplementation),
             abi.encodeCall(ReferralGateway.initializeNewVersion, (couponPool, couponRedeemer, 2))
@@ -121,13 +62,13 @@ contract CouponCodeTest is Test, SimulateDonResponse {
         vm.prank(couponPool);
         usdc.approve(address(referralGateway), 1000e6);
 
-        vm.prank(daoAdmin);
+        vm.prank(config.daoMultisig);
         referralGateway.createDAO(tDaoName, true, true, 1743479999, 1e12, address(bmConsumerMock));
     }
 
     /*//////////////////////////////////////////////////////////////
-                                    PREPAYS
-        //////////////////////////////////////////////////////////////*/
+                           COUPON PREPAYMENTS
+    //////////////////////////////////////////////////////////////*/
 
     //======== coupon higher than contribution ========//
     function testCouponPrepaymentCase1() public {
