c0e79c1bf1ac63bccbf099d44c63c9c84795f172
Maikel Ordaz <maikelordaz@gmail.com>
2024-12-26 17:08:36 -0400

    revise: fix deploy script

--- deploy/08-CcipSourceContract.s.sol
+++ deploy/08-CcipSourceContract.s.sol
@@ -10,6 +10,9 @@ contract DeployTokenTransferSource is Script {
     address public constant REFERRAL_MAINNET = 0x14Eb9897c6b7Ac579e6eFE130287e2729b9A018E;
     address public constant REFERRAL_TESTNET = 0x303892f65aD2862b496fd946E3827E71fcF88e47;
 
+    uint64 public constant ARB_ONE_CHAIN_SELECTOR = 4949039107694359620;
+    uint64 public constant ARB_SEPOLIA_CHAIN_SELECTOR = 3478487238524512106;
+
     function run() external returns (TokenTransferSource) {
         CcipHelperConfig ccipHelperConfig = new CcipHelperConfig();
 
@@ -18,10 +21,15 @@ contract DeployTokenTransferSource is Script {
         );
 
         address referralContract;
+        uint64 destinationChainSelector;
 
-        if (block.chainid == 421614 || block.chainid == 11155111)
+        if (block.chainid == 421614 || block.chainid == 11155111) {
             referralContract = REFERRAL_TESTNET;
-        else referralContract = REFERRAL_MAINNET;
+            destinationChainSelector = ARB_SEPOLIA_CHAIN_SELECTOR;
+        } else {
+            referralContract = REFERRAL_MAINNET;
+            destinationChainSelector = ARB_ONE_CHAIN_SELECTOR;
+        }
 
         vm.startBroadcast();
 
@@ -30,7 +38,8 @@ contract DeployTokenTransferSource is Script {
             config.router,
             config.link,
             config.usdc,
-            referralContract
+            referralContract,
+            destinationChainSelector
         );
 
         vm.stopBroadcast();
