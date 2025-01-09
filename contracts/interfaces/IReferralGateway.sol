80e3d7b3dfa67e745c4cf5fba4bd1e763f929294
Maikel Ordaz <maikelordaz@gmail.com>
2025-01-08 23:58:16 -0400

    chore: fix interface

--- contracts/interfaces/IReferralGateway.sol
+++ contracts/interfaces/IReferralGateway.sol
@@ -3,8 +3,5 @@
 pragma solidity 0.8.28;
 
 interface IReferralGateway {
-    function testCaller(
-        address caller,
-        uint256 callUint
-    ) external view returns (address, uint256, address);
+    function checkCaller(address caller, uint256 callUint) external;
 }
