70660e0dfe35e0a1c7a3e2fc8dd1698e434d798f
Maikel Ordaz <maikelordaz@gmail.com>
2025-01-09 19:28:58 -0400

    revise: low level call

--- contracts/chainlink/ccip/ReceiverContract.sol
+++ contracts/chainlink/ccip/ReceiverContract.sol
@@ -12,22 +12,11 @@ contract ReceiverContract is CCIPReceiver, Ownable2Step {
     using SafeERC20 for IERC20;
     using EnumerableMap for EnumerableMap.Bytes32ToUintMap;
 
-    error Receiver__InvalidUsdcToken();
-    error Receiver__InvalidSourceChain();
-    error Receiver__OnlySelf();
-    error Receiver__CallToStakerFailed();
-    error Receiver__MessageNotFailed(bytes32 messageId);
+    IERC20 private immutable usdc;
+    address private referralGateway;
 
-    event OnMessageReceived(
-        bytes32 indexed messageId,
-        uint64 indexed sourceChainSelector,
-        address indexed sender,
-        bytes data,
-        uint256 tokenAmount
-    );
-
-    event OnMessageFailed(bytes32 indexed messageId, bytes reason);
-    event OnMessageRecovered(bytes32 indexed messageId);
+    mapping(bytes32 messageId => Client.Any2EVMMessage message) public messageContentsById;
+    EnumerableMap.Bytes32ToUintMap internal failedMessages;
 
     enum ErrorCode {
         RESOLVED,
@@ -39,12 +28,21 @@ contract ReceiverContract is CCIPReceiver, Ownable2Step {
         ErrorCode errorCode;
     }
 
-    IERC20 private immutable usdc;
-    address private immutable referralGateway;
+    event OnMessageReceived(
+        bytes32 indexed messageId,
+        uint64 indexed sourceChainSelector,
+        address indexed sender,
+        bytes data,
+        uint256 tokenAmount
+    );
+    event OnMessageFailed(bytes32 indexed messageId, bytes reason);
+    event OnMessageRecovered(bytes32 indexed messageId);
 
-    mapping(bytes32 messageId => Client.Any2EVMMessage message) public messageContentsById;
-
-    EnumerableMap.Bytes32ToUintMap internal failedMessages;
+    error Receiver__InvalidUsdcToken();
+    error Receiver__InvalidSourceChain();
+    error Receiver__OnlySelf();
+    error Receiver__CallTFailed();
+    error Receiver__MessageNotFailed(bytes32 messageId);
 
     modifier validateSourceChain(uint64 _sourceChainSelector) {
         if (_sourceChainSelector == 0) revert Receiver__InvalidSourceChain();
@@ -68,7 +66,6 @@ contract ReceiverContract is CCIPReceiver, Ownable2Step {
         address _referralGateway
     ) CCIPReceiver(_router) Ownable(msg.sender) {
         require(_usdc != address(0), Receiver__InvalidUsdcToken());
-        // if (_referralGateway == address(0)) revert Receiver__InvalidStaker();
         usdc = IERC20(_usdc);
         referralGateway = _referralGateway;
         usdc.approve(_referralGateway, type(uint256).max);
@@ -78,12 +75,13 @@ contract ReceiverContract is CCIPReceiver, Ownable2Step {
      * @notice The entrypoint for the CCIP router to call. This function should
      *         never revert, all errors should be handled internally in this contract.
      * @param any2EvmMessage The message to process.
-     * @dev Extremely important to ensure only router calls this.
+     * @dev Only router can call
      */
     function ccipReceive(
         Client.Any2EVMMessage calldata any2EvmMessage
     ) external override onlyRouter {
         // Try-catch to avoid reverting the tx, emit event instead
+        // Try catch to be able to call the external function
         try this.processMessage(any2EvmMessage) {} catch (bytes memory err) {
             failedMessages.set(any2EvmMessage.messageId, uint256(ErrorCode.FAILED));
             messageContentsById[any2EvmMessage.messageId] = any2EvmMessage;
@@ -102,7 +100,7 @@ contract ReceiverContract is CCIPReceiver, Ownable2Step {
 
     function _ccipReceive(Client.Any2EVMMessage memory any2EvmMessage) internal override {
         (bool success, ) = referralGateway.call(any2EvmMessage.data);
-        require(success, Receiver__CallToStakerFailed());
+        require(success, Receiver__CallTFailed());
         emit OnMessageReceived(
             any2EvmMessage.messageId,
             any2EvmMessage.sourceChainSelector,
@@ -114,12 +112,12 @@ contract ReceiverContract is CCIPReceiver, Ownable2Step {
 
     /**
      * @notice Allows the owner to retry a failed message in order to unblock the associated tokens.
-     * @param messageId The unique identifier of the failed message.
-     * @param beneficiary The address to which the tokens will be sent.
-     * @dev This function is only callable by the contract owner. It changes the status of the message
-     *      from 'failed' to 'resolved' to prevent reentry and multiple retries of the same message.
+     * @param messageId The unique identifier of the failed message
+     * @dev This function is only callable by the contract owner
+     * @dev It changes the status of the message from 'failed' to 'resolved' to prevent reentrancy
+     * Todo: Try again the low level call to the referral gateway
      */
-    function retryFailedMessage(bytes32 messageId, address beneficiary) external onlyOwner {
+    function retryFailedMessage(bytes32 messageId) external onlyOwner {
         require(
             failedMessages.get(messageId) == uint256(ErrorCode.FAILED),
             Receiver__MessageNotFailed(messageId)
@@ -128,20 +126,14 @@ contract ReceiverContract is CCIPReceiver, Ownable2Step {
 
         Client.Any2EVMMessage memory message = messageContentsById[messageId];
 
-        IERC20(message.destTokenAmounts[0].token).safeTransfer(
-            beneficiary,
-            message.destTokenAmounts[0].amount
-        );
-
         emit OnMessageRecovered(messageId);
     }
 
     /**
      * @notice Retrieves a paginated list of failed messages.
-     * @dev This function returns a subset of failed messages defined by `offset` and `limit` parameters. It ensures that the pagination parameters are within the bounds of the available data set.
-     * @param offset The index of the first failed message to return, enabling pagination by skipping a specified number of messages from the start of the dataset.
-     * @param limit The maximum number of failed messages to return, restricting the size of the returned array.
-     * @return failedMessages An array of `FailedMessage` struct, each containing a `messageId` and an `errorCode` (RESOLVED or FAILED), representing the requested subset of failed messages. The length of the returned array is determined by the `limit` and the total number of failed messages.
+     * @param offset The index of the first failed message to return, enabling pagination by skipping a specified number of messages
+     * @param limit The maximum number of failed messages to return, this is the size of the returned array
+     * @return failedMessages. Array of `FailedMessage` struct, with a `messageId` and an `errorCode` (RESOLVED or FAILED)
      */
     function getFailedMessages(
         uint256 offset,
