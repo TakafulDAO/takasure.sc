6b8839d89866191ce349c8e6d7140e1175b25d99
Maikel Ordaz <maikelordaz@gmail.com>
2025-01-08 23:01:58 -0400

    revise: deploy scripts

---
+++ deploy/09-CcipSourceTransferAndCall.s.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: GNU GPLv3
+
+pragma solidity 0.8.28;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {TransferAndCallSource} from "contracts/chainlink/ccip/TransferAndCallSource.sol";
+import {CcipHelperConfig} from "./CcipHelperConfig.s.sol";
+
+contract DeployTransferAndCallSource is Script {
+    uint256 public constant AVAX_FUJI_CHAIN_ID = 43113;
+    uint256 public constant BASE_SEPOLIA_CHAIN_ID = 84532;
+    uint256 public constant ETH_SEPOLIA_CHAIN_ID = 11155111;
+    uint256 public constant OP_SEPOLIA_CHAIN_ID = 11155420;
+    uint256 public constant POL_AMOY_CHAIN_ID = 80002;
+
+    address public constant RECEIVER_MAINNET = 0x14Eb9897c6b7Ac579e6eFE130287e2729b9A018E; //TODO: deploy first
+    address public constant RECEIVER_TESTNET = 0x7444908f54F304CC5F6e97d809C106B33953036C;
+
+    uint64 public constant ARB_ONE_CHAIN_SELECTOR = 4949039107694359620;
+    uint64 public constant ARB_SEPOLIA_CHAIN_SELECTOR = 3478487238524512106;
+
+    function run() external returns (TransferAndCallSource) {
+        uint256 chainId = block.chainid;
+
+        CcipHelperConfig ccipHelperConfig = new CcipHelperConfig();
+
+        CcipHelperConfig.CCIPNetworkConfig memory config = ccipHelperConfig.getConfigByChainId(
+            block.chainid
+        );
+
+        address receiverContract;
+        uint64 destinationChainSelector;
+        bytes32 salt = "10031960";
+
+        if (
+            chainId == AVAX_FUJI_CHAIN_ID ||
+            chainId == BASE_SEPOLIA_CHAIN_ID ||
+            chainId == ETH_SEPOLIA_CHAIN_ID ||
+            chainId == OP_SEPOLIA_CHAIN_ID ||
+            chainId == POL_AMOY_CHAIN_ID
+        ) {
+            receiverContract = RECEIVER_TESTNET;
+            destinationChainSelector = ARB_SEPOLIA_CHAIN_SELECTOR;
+        } else {
+            receiverContract = RECEIVER_MAINNET;
+            destinationChainSelector = ARB_ONE_CHAIN_SELECTOR;
+        }
+
+        vm.startBroadcast();
+
+        // Deploy TransferAndCallSource contract
+        TransferAndCallSource transferAndCallSource = new TransferAndCallSource{salt: salt}(
+            config.router,
+            config.link,
+            config.usdc,
+            receiverContract,
+            destinationChainSelector
+        );
+
+        vm.stopBroadcast();
+
+        return (transferAndCallSource);
+    }
+}
---
+++ deploy/10-CcipReceiver.s.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: GNU GPLv3
+
+pragma solidity 0.8.28;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {ReceiverContract} from "contracts/chainlink/ccip/ReceiverContract.sol";
+import {CcipHelperConfig} from "./CcipHelperConfig.s.sol";
+
+contract DeployReceiverContract is Script {
+    uint256 public constant AVAX_FUJI_CHAIN_ID = 43113;
+    uint256 public constant BASE_SEPOLIA_CHAIN_ID = 84532;
+    uint256 public constant ETH_SEPOLIA_CHAIN_ID = 11155111;
+    uint256 public constant OP_SEPOLIA_CHAIN_ID = 11155420;
+    uint256 public constant POL_AMOY_CHAIN_ID = 80002;
+
+    address public constant REFERRAL_MAINNET = 0x14Eb9897c6b7Ac579e6eFE130287e2729b9A018E;
+    address public constant REFERRAL_TESTNET = 0x303892f65aD2862b496fd946E3827E71fcF88e47;
+
+    uint64 public constant ARB_ONE_CHAIN_SELECTOR = 4949039107694359620;
+    uint64 public constant ARB_SEPOLIA_CHAIN_SELECTOR = 3478487238524512106;
+
+    function run() external returns (ReceiverContract) {
+        uint256 chainId = block.chainid;
+
+        CcipHelperConfig ccipHelperConfig = new CcipHelperConfig();
+
+        CcipHelperConfig.CCIPNetworkConfig memory config = ccipHelperConfig.getConfigByChainId(
+            block.chainid
+        );
+
+        address referralContract;
+        bytes32 salt = "10031960";
+
+        if (
+            chainId == AVAX_FUJI_CHAIN_ID ||
+            chainId == BASE_SEPOLIA_CHAIN_ID ||
+            chainId == ETH_SEPOLIA_CHAIN_ID ||
+            chainId == OP_SEPOLIA_CHAIN_ID ||
+            chainId == POL_AMOY_CHAIN_ID
+        ) {
+            referralContract = REFERRAL_TESTNET;
+        } else {
+            referralContract = REFERRAL_MAINNET;
+        }
+
+        vm.startBroadcast();
+
+        // Deploy TransferAndCallSource contract
+        ReceiverContract receiverContract = new ReceiverContract{salt: salt}(
+            config.router,
+            config.usdc,
+            referralContract
+        );
+
+        vm.stopBroadcast();
+
+        return (receiverContract);
+    }
+}
