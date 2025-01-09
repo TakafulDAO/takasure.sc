640aaca89288a960d57299a3ba7ef096117eb644
Maikel Ordaz <maikelordaz@gmail.com>
2025-01-08 23:02:31 -0400

    revise: interaction scripts

--- scripts/contract-interactions/chainlink-ccip/TransferAndCallUsdcPayWithLink.s.sol
+++ scripts/contract-interactions/chainlink-ccip/TransferAndCallUsdcPayWithLink.s.sol
@@ -3,11 +3,11 @@
 pragma solidity 0.8.28;
 
 import {Script, console2, GetContractAddress} from "scripts/utils/GetContractAddress.s.sol";
-import {TokenTransferSource} from "contracts/chainlink/ccip/TokenTransferSource.sol";
+import {TransferAndCallSource} from "contracts/chainlink/ccip/TransferAndCallSource.sol";
 import {CcipHelperConfig} from "deploy/CcipHelperConfig.s.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract TransferUsdcPayWithLink is Script, GetContractAddress {
+contract TransferAndCallUsdcPayWithLink is Script, GetContractAddress {
     function run() public {
         uint256 chainId = block.chainid;
         uint256 amountToApprove = 1e6; // 1 USDC
@@ -18,17 +18,20 @@ contract TransferUsdcPayWithLink is Script, GetContractAddress {
             chainId
         );
 
-        address tokenTransferSourceAddress = _getContractAddress(chainId, "TokenTransferSource");
-        TokenTransferSource tokenTransferSource = TokenTransferSource(
-            payable(tokenTransferSourceAddress)
+        address transferAndCallSourceAddress = _getContractAddress(
+            chainId,
+            "TransferAndCallSource"
+        );
+        TransferAndCallSource transferAndCallSource = TransferAndCallSource(
+            payable(transferAndCallSourceAddress)
         );
 
         IERC20 usdc = IERC20(config.usdc);
 
         vm.startBroadcast();
 
-        usdc.approve(address(tokenTransferSource), amountToApprove);
-        tokenTransferSource.transferUSDCPayLINK(amountToApprove);
+        usdc.approve(address(transferAndCallSource), amountToApprove);
+        transferAndCallSource.transferUSDCPayLINK(amountToApprove);
 
         vm.stopBroadcast();
     }
