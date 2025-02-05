78bf00043bd68b3601755808887635a193948bd8
Maikel Ordaz <maikelordaz@gmail.com>
2025-02-05 16:51:39 -0400

    chore: create ModuleManager

---
+++ contracts/takasure/modules/ModuleManager.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: GPL-3.0
+
+/**
+ * @title ModuleManager
+ * @author Maikel Ordaz
+ * @notice This contract will manage the modules of the Takasure protocol
+ * @dev The logic in this contract will have three main purposes:
+ *      1. Add/Remove new modules to the protocol
+ *      2. Pause/Unpause or Deprecate modules in the protocol
+ *      3. Comunicate with the core protocol contracts if the module is paused or not
+ * @dev The state in this contract will be mainly the modules addresses and their status and any other auxiliary data
+ */
+
+pragma solidity 0.8.28;
+
+contract ModuleManager {}
