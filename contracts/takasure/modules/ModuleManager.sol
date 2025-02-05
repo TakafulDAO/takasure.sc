7c4e64cf51434bdbf1aa9b75f5ff62e5a9daa6a6
Maikel Ordaz <maikelordaz@gmail.com>
2025-02-05 17:17:51 -0400

    revise: addModule checks

--- contracts/takasure/modules/ModuleManager.sol
+++ contracts/takasure/modules/ModuleManager.sol
@@ -6,18 +6,20 @@
  * @notice This contract will manage the modules of the Takasure protocol
  * @dev The logic in this contract will have three main purposes:
  *      1. Add/Remove new modules to the protocol
- *      2. Pause/Unpause or Deprecate modules in the protocol
+ *      2. Disable/Enable, Pause/Unpause or Deprecate modules in the protocol
  *      3. Comunicate with the core protocol contracts if the module is paused or not
  * @dev The state in this contract will be mainly the modules addresses and their status and any other auxiliary data
  */
 
 import {Ownable2Step, Ownable} from "@openzeppelin/contracts/access/Ownable2Step.sol";
+import {ReentrancyGuardTransient} from "@openzeppelin/contracts/utils/ReentrancyGuardTransient.sol";
 
 pragma solidity 0.8.28;
 
-contract ModuleManager is Ownable2Step {
+contract ModuleManager is Ownable2Step, ReentrancyGuardTransient {
     enum Status {
-        ACTIVE,
+        DISABLED,
+        ENABLED,
         PAUSED,
         DEPRECATED
     }
@@ -28,5 +30,27 @@ contract ModuleManager is Ownable2Step {
 
     mapping(address moduleAddr => Module) private addressToModule;
 
+    error ModuleManager__AddressZeroNotAllowed();
+    error ModuleManager__AlreadyModule();
+    error ModuleManager__WrongInitialState();
+
     constructor() Ownable(msg.sender) {}
+
+    /**
+     * @notice A function to add a new module
+     * @param newModule The new module address
+     * @param status Can only be DISABLED or ENABLED
+     */
+    function addModule(address newModule, Status status) external onlyOwner nonReentrant {\
+        // New module can not be address 0, can not be already a module, and the status must be disabled or enabled
+        require(newModule != address(0), ModuleManager__AddressZeroNotAllowed());
+        require(
+            addressToModule[newModule].moduleAddress == address(0),
+            ModuleManager__AlreadyModule()
+        );
+        require(
+            status == Status.DISABLED || status == Status.ENABLED,
+            ModuleManager__WrongInitialState()
+        );
+    }
 }
