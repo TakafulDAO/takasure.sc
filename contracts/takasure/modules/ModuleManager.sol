f1ed09409393d1e5348055559e44da0be8f66c1f
Maikel Ordaz <maikelordaz@gmail.com>
2025-02-05 18:55:03 -0400

    revise: finishh module manager contract

--- contracts/takasure/modules/ModuleManager.sol
+++ contracts/takasure/modules/ModuleManager.sol
@@ -11,13 +11,14 @@
  * @dev The state in this contract will be mainly the modules addresses and their status and any other auxiliary data
  */
 
+import {IModuleCheck} from "contracts/interfaces/IModuleCheck.sol";
 import {Ownable2Step, Ownable} from "@openzeppelin/contracts/access/Ownable2Step.sol";
 import {ReentrancyGuardTransient} from "@openzeppelin/contracts/utils/ReentrancyGuardTransient.sol";
 
 pragma solidity 0.8.28;
 
 contract ModuleManager is Ownable2Step, ReentrancyGuardTransient {
-    enum Status {
+    enum State {
         DISABLED,
         ENABLED,
         PAUSED,
@@ -25,25 +26,36 @@ contract ModuleManager is Ownable2Step, ReentrancyGuardTransient {
     }
     struct Module {
         address moduleAddress;
-        Status moduleState;
+        State moduleState;
     }
 
     mapping(address moduleAddr => Module) private addressToModule;
 
-    event OnNewModule(address newModuleAddr, Status newModuleStatus);
+    /*//////////////////////////////////////////////////////////////
+                            EVENT AND ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    event OnNewModule(address newModuleAddr, State newModuleStatus);
+    event OnModuleStateChanged(State oldState, State newState);
 
     error ModuleManager__AddressZeroNotAllowed();
     error ModuleManager__AlreadyModule();
     error ModuleManager__WrongInitialState();
+    error ModuleManager__NotModule();
+    error ModuleManager__WrongState();
 
     constructor() Ownable(msg.sender) {}
+
+    /*//////////////////////////////////////////////////////////////
+                      MODULE MANAGEMENT FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
 
     /**
      * @notice A function to add a new module
      * @param newModule The new module address
-     * @param status Can only be DISABLED or ENABLED
+     * @param state Can only be DISABLED or ENABLED
      */
-    function addModule(address newModule, Status status) external onlyOwner nonReentrant {
+    function addModule(address newModule, State state) external onlyOwner nonReentrant {
         // New module can not be address 0, can not be already a module, and the status must be disabled or enabled
         require(newModule != address(0), ModuleManager__AddressZeroNotAllowed());
         require(
@@ -51,13 +63,64 @@ contract ModuleManager is Ownable2Step, ReentrancyGuardTransient {
             ModuleManager__AlreadyModule()
         );
         require(
-            status == Status.DISABLED || status == Status.ENABLED,
+            state == State.DISABLED || state == State.ENABLED,
             ModuleManager__WrongInitialState()
         );
 
         addressToModule[newModule].moduleAddress = newModule;
-        addressToModule[newModule].moduleState = status;
+        addressToModule[newModule].moduleState = state;
 
-        emit OnNewModule(newModule, status);
+        emit OnNewModule(newModule, state);
+    }
+
+    /**
+     * @notice A function to change the state of a module
+     * @param module The module address to change the state
+     * @param newState The new state of the module
+     * @dev The module can not be DEPRECATED
+     */
+    function changeModuleStatus(address module, State newState) external onlyOwner {
+        require(addressToModule[module].moduleAddress != address(0), ModuleManager__NotModule());
+        require(module != address(0), ModuleManager__AddressZeroNotAllowed());
+        require(
+            addressToModule[module].moduleState != State.DEPRECATED,
+            ModuleManager__WrongState()
+        );
+
+        State oldState = addressToModule[module].moduleState;
+        addressToModule[module].moduleState = newState;
+
+        emit OnModuleStateChanged(oldState, newState);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                           GETTERS FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Return the module state
+     * @param module The module address
+     * @return The state of the module
+     * @dev The given address must be a module
+     */
+    function getModuleState(address module) external view returns (State) {
+        require(addressToModule[module].moduleAddress != address(0), ModuleManager__NotModule());
+        return addressToModule[module].moduleState;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                           INTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Check if a given address is a module
+     */
+    function _checkIsModule(address _newModule) internal {
+        try IModuleCheck(_newModule).isTLDModule() returns (bytes4 funcSelector) {
+            if (funcSelector != IModuleCheck.isTLDModule.selector)
+                revert ModuleManager__NotModule();
+        } catch (bytes memory reason) {
+            if (reason.length == 0) revert ModuleManager__NotModule();
+        }
     }
 }
