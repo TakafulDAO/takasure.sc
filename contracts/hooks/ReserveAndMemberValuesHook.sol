512cadad128f1538acccd7b9acde057fd8b2c688
Maikel Ordaz <maikelordaz@gmail.com>
2025-02-06 21:55:07 -0400

    chore: comment

--- contracts/hooks/ReserveAndMemberValuesHook.sol
+++ contracts/hooks/ReserveAndMemberValuesHook.sol
@@ -4,6 +4,7 @@
  * @title ReserveAndMemberValuesHook
  * @author Maikel Ordaz
  * @notice This contract will help the modules to set new values in the reserve contract
+ * @dev This functions are intended to be called before and after certain actions in the Core contracts
  * @dev The logic in this contract will have one main purpose, and for that it will follow the next steps:
  *      1. Get the current values of the reserve and/or a given member from the Core contracts
  *      2. Set new values for the reserve and/or a given member in the Core contracts
