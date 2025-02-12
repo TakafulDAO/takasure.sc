8eb3f572399a471660a12ad67ba12dcfe660afda
Maikel Ordaz <maikelordaz@gmail.com>
2025-02-11 22:01:19 -0400

    chore: rearrange tests

---
+++ test/unit/03-modules/01-prejoin-module/07-Roles_PrejoinModuleTest.t.sol
@@ -0,0 +1,206 @@
+// SPDX-License-Identifier: GNU GPLv3
+
+pragma solidity 0.8.28;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {TestDeployProtocol} from "test/utils/TestDeployProtocol.s.sol";
+import {PrejoinModule} from "contracts/modules/PrejoinModule.sol";
+import {TakasureReserve} from "contracts/core/TakasureReserve.sol";
+import {EntryModule} from "contracts/modules/EntryModule.sol";
+import {BenefitMultiplierConsumerMock} from "test/mocks/BenefitMultiplierConsumerMock.sol";
+import {HelperConfig} from "deploy/utils/configs/HelperConfig.s.sol";
+import {IUSDC} from "test/mocks/IUSDCmock.sol";
+
+contract RolesPrejoinModuleTest is Test {
+    TestDeployProtocol deployer;
+    PrejoinModule prejoinModule;
+    TakasureReserve takasureReserve;
+    EntryModule entryModule;
+    BenefitMultiplierConsumerMock bmConsumerMock;
+    HelperConfig helperConfig;
+    IUSDC usdc;
+    address usdcAddress;
+    address prejoinModuleAddress;
+    address takasureReserveAddress;
+    address entryModuleAddress;
+    address takadao;
+    address daoAdmin;
+    address KYCProvider;
+    address referral = makeAddr("referral");
+    address member = makeAddr("member");
+    address notMember = makeAddr("notMember");
+    address child = makeAddr("child");
+    string tDaoName = "TheLifeDao";
+    uint256 public constant USDC_INITIAL_AMOUNT = 100e6; // 100 USDC
+    uint256 public constant CONTRIBUTION_AMOUNT = 25e6; // 25 USDC
+
+    function setUp() public {
+        // Deployer
+        deployer = new TestDeployProtocol();
+        // Deploy contracts
+        (
+            ,
+            bmConsumerMock,
+            takasureReserveAddress,
+            prejoinModuleAddress,
+            entryModuleAddress,
+            ,
+            ,
+            ,
+            usdcAddress,
+            ,
+            helperConfig
+        ) = deployer.run();
+
+        // Get config values
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfigByChainId(block.chainid);
+        takadao = config.takadaoOperator;
+        daoAdmin = config.daoMultisig;
+        KYCProvider = config.kycProvider;
+
+        // Assign implementations
+        prejoinModule = PrejoinModule(prejoinModuleAddress);
+        takasureReserve = TakasureReserve(takasureReserveAddress);
+        entryModule = EntryModule(entryModuleAddress);
+        usdc = IUSDC(usdcAddress);
+
+        // Config mocks
+        vm.startPrank(daoAdmin);
+        takasureReserve.setNewContributionToken(address(usdc));
+        takasureReserve.setNewBenefitMultiplierConsumerAddress(address(bmConsumerMock));
+        takasureReserve.setNewReferralGateway(address(prejoinModule));
+        vm.stopPrank();
+
+        vm.startPrank(bmConsumerMock.admin());
+        bmConsumerMock.setNewRequester(address(takasureReserve));
+        bmConsumerMock.setNewRequester(prejoinModuleAddress);
+        vm.stopPrank();
+
+        // Give and approve USDC
+        deal(address(usdc), referral, USDC_INITIAL_AMOUNT);
+        deal(address(usdc), child, USDC_INITIAL_AMOUNT);
+        deal(address(usdc), member, USDC_INITIAL_AMOUNT);
+
+        vm.prank(referral);
+        usdc.approve(address(prejoinModule), USDC_INITIAL_AMOUNT);
+        vm.prank(child);
+        usdc.approve(address(prejoinModule), USDC_INITIAL_AMOUNT);
+        vm.prank(member);
+        usdc.approve(address(takasureReserve), USDC_INITIAL_AMOUNT);
+    }
+
+    modifier createDao() {
+        vm.startPrank(daoAdmin);
+        prejoinModule.createDAO(tDaoName, true, true, 1743479999, 1e12, address(bmConsumerMock));
+        prejoinModule.setDAOName(tDaoName);
+        vm.stopPrank();
+        _;
+    }
+
+    modifier referralPrepays() {
+        vm.prank(referral);
+        prejoinModule.payContribution(CONTRIBUTION_AMOUNT, address(0));
+        _;
+    }
+
+    modifier KYCReferral() {
+        vm.prank(KYCProvider);
+        prejoinModule.setKYCStatus(referral);
+        _;
+    }
+
+    modifier referredPrepays() {
+        vm.prank(child);
+        prejoinModule.payContribution(CONTRIBUTION_AMOUNT, referral);
+
+        _;
+    }
+
+    function testRoles() public createDao referralPrepays KYCReferral referredPrepays {
+        // Addresses that will be used to test the roles
+        address newOperator = makeAddr("newOperator");
+        address newKYCProvider = makeAddr("newKYCProvider");
+        // Current addresses with roles
+        assert(prejoinModule.hasRole(keccak256("OPERATOR"), takadao));
+        assert(prejoinModule.hasRole(keccak256("KYC_PROVIDER"), KYCProvider));
+        // New addresses without roles
+        assert(!prejoinModule.hasRole(keccak256("OPERATOR"), newOperator));
+        assert(!prejoinModule.hasRole(keccak256("KYC_PROVIDER"), newKYCProvider));
+        // Current KYCProvider can KYC a member
+        vm.prank(KYCProvider);
+        prejoinModule.setKYCStatus(child);
+        // Grant, revoke and renounce roles
+        vm.startPrank(takadao);
+        prejoinModule.grantRole(keccak256("OPERATOR"), newOperator);
+        prejoinModule.grantRole(keccak256("KYC_PROVIDER"), newKYCProvider);
+        prejoinModule.revokeRole(keccak256("OPERATOR"), takadao);
+        prejoinModule.revokeRole(keccak256("KYC_PROVIDER"), KYCProvider);
+        vm.stopPrank();
+        // New addresses with roles
+        assert(prejoinModule.hasRole(keccak256("OPERATOR"), newOperator));
+        assert(prejoinModule.hasRole(keccak256("KYC_PROVIDER"), newKYCProvider));
+        // Old addresses without roles
+        assert(!prejoinModule.hasRole(keccak256("OPERATOR"), takadao));
+        assert(!prejoinModule.hasRole(keccak256("KYC_PROVIDER"), KYCProvider));
+    }
+
+    function testAdminRole() public createDao referralPrepays KYCReferral referredPrepays {
+        // Address that will be used to test the roles
+        address newAdmin = makeAddr("newAdmin");
+        address newCouponRedeemer = makeAddr("newCouponRedeemer");
+
+        bytes32 defaultAdminRole = 0x00;
+        bytes32 couponRedeemer = keccak256("COUPON_REDEEMER");
+
+        // Current address with roles
+        assert(prejoinModule.hasRole(defaultAdminRole, takadao));
+
+        // New addresses without roles
+        assert(!prejoinModule.hasRole(defaultAdminRole, newAdmin));
+
+        // Current Admin can give and remove anyone a role
+        vm.prank(takadao);
+        prejoinModule.grantRole(couponRedeemer, newCouponRedeemer);
+
+        assert(prejoinModule.hasRole(couponRedeemer, newCouponRedeemer));
+
+        vm.prank(takadao);
+        prejoinModule.revokeRole(couponRedeemer, newCouponRedeemer);
+
+        assert(!prejoinModule.hasRole(couponRedeemer, newCouponRedeemer));
+
+        // Grant, revoke and renounce roles
+        vm.startPrank(takadao);
+        prejoinModule.grantRole(defaultAdminRole, newAdmin);
+        prejoinModule.renounceRole(defaultAdminRole, takadao);
+        vm.stopPrank();
+
+        // New addresses with roles
+        assert(prejoinModule.hasRole(defaultAdminRole, newAdmin));
+
+        // Old addresses without roles
+        assert(!prejoinModule.hasRole(defaultAdminRole, takadao));
+
+        // New Admin can give and remove anyone a role
+        vm.prank(newAdmin);
+        prejoinModule.grantRole(couponRedeemer, newCouponRedeemer);
+
+        assert(prejoinModule.hasRole(couponRedeemer, newCouponRedeemer));
+
+        vm.prank(newAdmin);
+        prejoinModule.revokeRole(couponRedeemer, newCouponRedeemer);
+
+        assert(!prejoinModule.hasRole(couponRedeemer, newCouponRedeemer));
+
+        // Old Admin can no longer give anyone a role
+        vm.prank(takadao);
+        vm.expectRevert(
+            abi.encodeWithSignature(
+                "AccessControlUnauthorizedAccount(address,bytes32)",
+                takadao,
+                defaultAdminRole
+            )
+        );
+        prejoinModule.grantRole(couponRedeemer, newCouponRedeemer);
+    }
+}
--- test/unit/04-referral-gateway/CouponCodeTest.t.sol
+++ test/unit/04-referral-gateway/CouponCodeTest.t.sol
@@ -1,204 +1,204 @@
-// // SPDX-License-Identifier: GNU GPLv3
+// SPDX-License-Identifier: GNU GPLv3
 
-// pragma solidity 0.8.28;
+pragma solidity 0.8.28;
 
-// import {Test, console2} from "forge-std/Test.sol";
-// import {TestDeployTakasureReserve} from "test/utils/TestDeployTakasureReserve.s.sol";
-// import {ReferralGateway} from "contracts/referrals/ReferralGateway.sol";
-// import {BenefitMultiplierConsumerMock} from "test/mocks/BenefitMultiplierConsumerMock.sol";
-// import {HelperConfig} from "deploy/utils/configs/HelperConfig.s.sol";
-// import {IUSDC} from "test/mocks/IUSDCmock.sol";
+import {Test, console2} from "forge-std/Test.sol";
+import {TestDeployTakasureReserve} from "test/utils/TestDeployTakasureReserve.s.sol";
+import {ReferralGateway} from "contracts/referrals/ReferralGateway.sol";
+import {BenefitMultiplierConsumerMock} from "test/mocks/BenefitMultiplierConsumerMock.sol";
+import {HelperConfig} from "deploy/utils/configs/HelperConfig.s.sol";
+import {IUSDC} from "test/mocks/IUSDCmock.sol";
 
-// contract CouponCodeTest is Test {
-//     TestDeployTakasureReserve deployer;
-//     ReferralGateway referralGateway;
-//     BenefitMultiplierConsumerMock bmConsumerMock;
-//     HelperConfig helperConfig;
-//     IUSDC usdc;
-//     address usdcAddress;
-//     address referralGatewayAddress;
-//     address operator;
-//     address child = makeAddr("child");
-//     address couponPool = makeAddr("couponPool");
-//     address couponRedeemer = makeAddr("couponRedeemer");
-//     string tDaoName = "TheLifeDao";
-//     uint256 public constant USDC_INITIAL_AMOUNT = 100e6; // 100 USDC
-//     uint256 public constant CONTRIBUTION_AMOUNT = 25e6; // 25 USDC
-//     uint256 public constant CONTRIBUTION_PREJOIN_DISCOUNT_RATIO = 10; // 10% of contribution deducted from fee
+contract CouponCodeTest is Test {
+    TestDeployTakasureReserve deployer;
+    ReferralGateway referralGateway;
+    BenefitMultiplierConsumerMock bmConsumerMock;
+    HelperConfig helperConfig;
+    IUSDC usdc;
+    address usdcAddress;
+    address referralGatewayAddress;
+    address operator;
+    address child = makeAddr("child");
+    address couponPool = makeAddr("couponPool");
+    address couponRedeemer = makeAddr("couponRedeemer");
+    string tDaoName = "TheLifeDao";
+    uint256 public constant USDC_INITIAL_AMOUNT = 100e6; // 100 USDC
+    uint256 public constant CONTRIBUTION_AMOUNT = 25e6; // 25 USDC
+    uint256 public constant CONTRIBUTION_PREJOIN_DISCOUNT_RATIO = 10; // 10% of contribution deducted from fee
 
-//     event OnNewCouponPoolAddress(address indexed oldCouponPool, address indexed newCouponPool);
-//     event OnCouponRedeemed(
-//         address indexed member,
-//         string indexed tDAOName,
-//         uint256 indexed couponAmount
-//     );
+    event OnNewCouponPoolAddress(address indexed oldCouponPool, address indexed newCouponPool);
+    event OnCouponRedeemed(
+        address indexed member,
+        string indexed tDAOName,
+        uint256 indexed couponAmount
+    );
 
-//     function setUp() public {
-//         // Deployer
-//         deployer = new TestDeployTakasureReserve();
-//         // Deploy contracts
-//         (, bmConsumerMock, , , , , , referralGatewayAddress, usdcAddress, , helperConfig) = deployer
-//             .run();
+    function setUp() public {
+        // Deployer
+        deployer = new TestDeployTakasureReserve();
+        // Deploy contracts
+        (, bmConsumerMock, , , , , , referralGatewayAddress, usdcAddress, , helperConfig) = deployer
+            .run();
 
-//         // Get config values
-//         HelperConfig.NetworkConfig memory config = helperConfig.getConfigByChainId(block.chainid);
-//         operator = config.takadaoOperator;
+        // Get config values
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfigByChainId(block.chainid);
+        operator = config.takadaoOperator;
 
-//         // Assign implementations
-//         referralGateway = ReferralGateway(address(referralGatewayAddress));
-//         usdc = IUSDC(usdcAddress);
+        // Assign implementations
+        referralGateway = ReferralGateway(address(referralGatewayAddress));
+        usdc = IUSDC(usdcAddress);
 
-//         // Give and approve USDC
-//         deal(address(usdc), child, USDC_INITIAL_AMOUNT);
-//         vm.prank(child);
-//         usdc.approve(address(referralGateway), USDC_INITIAL_AMOUNT);
+        // Give and approve USDC
+        deal(address(usdc), child, USDC_INITIAL_AMOUNT);
+        vm.prank(child);
+        usdc.approve(address(referralGateway), USDC_INITIAL_AMOUNT);
 
-//         deal(address(usdc), couponPool, 1000e6);
+        deal(address(usdc), couponPool, 1000e6);
 
-//         vm.prank(couponPool);
-//         usdc.approve(address(referralGateway), 1000e6);
+        vm.prank(couponPool);
+        usdc.approve(address(referralGateway), 1000e6);
 
-//         vm.prank(config.daoMultisig);
-//         referralGateway.createDAO(tDaoName, true, true, 1743479999, 1e12, address(bmConsumerMock));
+        vm.prank(config.daoMultisig);
+        referralGateway.createDAO(tDaoName, true, true, 1743479999, 1e12, address(bmConsumerMock));
 
-//         vm.prank(bmConsumerMock.admin());
-//         bmConsumerMock.setNewRequester(referralGatewayAddress);
-//     }
+        vm.prank(bmConsumerMock.admin());
+        bmConsumerMock.setNewRequester(referralGatewayAddress);
+    }
 
-//     function testSetNewCouponPoolAddress() public {
-//         vm.prank(operator);
-//         vm.expectEmit(true, true, false, false, address(referralGateway));
-//         emit OnNewCouponPoolAddress(address(0), couponPool);
-//         referralGateway.setCouponPoolAddress(couponPool);
-//     }
+    function testSetNewCouponPoolAddress() public {
+        vm.prank(operator);
+        vm.expectEmit(true, true, false, false, address(referralGateway));
+        emit OnNewCouponPoolAddress(address(0), couponPool);
+        referralGateway.setCouponPoolAddress(couponPool);
+    }
 
-//     modifier setCouponPoolAndCouponRedeemer() {
-//         vm.startPrank(operator);
-//         referralGateway.setCouponPoolAddress(couponPool);
-//         referralGateway.grantRole(keccak256("COUPON_REDEEMER"), couponRedeemer);
-//         vm.stopPrank();
-//         _;
-//     }
+    modifier setCouponPoolAndCouponRedeemer() {
+        vm.startPrank(operator);
+        referralGateway.setCouponPoolAddress(couponPool);
+        referralGateway.grantRole(keccak256("COUPON_REDEEMER"), couponRedeemer);
+        vm.stopPrank();
+        _;
+    }
 
-//     /*//////////////////////////////////////////////////////////////
-//                            COUPON PREPAYMENTS
-//     //////////////////////////////////////////////////////////////*/
+    /*//////////////////////////////////////////////////////////////
+                           COUPON PREPAYMENTS
+    //////////////////////////////////////////////////////////////*/
 
-//     //======== coupon higher than contribution ========//
-//     function testCouponPrepaymentCase1() public setCouponPoolAndCouponRedeemer {
-//         uint256 couponAmount = CONTRIBUTION_AMOUNT * 2;
+    //======== coupon higher than contribution ========//
+    function testCouponPrepaymentCase1() public setCouponPoolAndCouponRedeemer {
+        uint256 couponAmount = CONTRIBUTION_AMOUNT * 2;
 
-//         uint256 initialCouponPoolBalance = usdc.balanceOf(couponPool);
-//         uint256 initialOperatorBalance = usdc.balanceOf(operator);
+        uint256 initialCouponPoolBalance = usdc.balanceOf(couponPool);
+        uint256 initialOperatorBalance = usdc.balanceOf(operator);
 
-//         vm.prank(couponRedeemer);
-//         vm.expectEmit(true, true, true, false, address(referralGateway));
-//         emit OnCouponRedeemed(child, tDaoName, couponAmount);
-//         (uint256 feeToOp, ) = referralGateway.payContributionOnBehalfOf(
-//             CONTRIBUTION_AMOUNT,
-//             tDaoName,
-//             address(0),
-//             child,
-//             couponAmount
-//         );
+        vm.prank(couponRedeemer);
+        vm.expectEmit(true, true, true, false, address(referralGateway));
+        emit OnCouponRedeemed(child, tDaoName, couponAmount);
+        (uint256 feeToOp, ) = referralGateway.payContributionOnBehalfOf(
+            CONTRIBUTION_AMOUNT,
+            tDaoName,
+            address(0),
+            child,
+            couponAmount
+        );
 
-//         uint256 finalCouponPoolBalance = usdc.balanceOf(couponPool);
-//         uint256 finalOperatorBalance = usdc.balanceOf(operator);
+        uint256 finalCouponPoolBalance = usdc.balanceOf(couponPool);
+        uint256 finalOperatorBalance = usdc.balanceOf(operator);
 
-//         assertEq(finalCouponPoolBalance, initialCouponPoolBalance - couponAmount);
-//         assertEq(finalOperatorBalance, initialOperatorBalance + feeToOp);
-//         assert(feeToOp > 0);
+        assertEq(finalCouponPoolBalance, initialCouponPoolBalance - couponAmount);
+        assertEq(finalOperatorBalance, initialOperatorBalance + feeToOp);
+        assert(feeToOp > 0);
 
-//         (uint256 contributionBeforeFee, , , uint256 discount) = referralGateway.getPrepaidMember(
-//             child,
-//             tDaoName
-//         );
+        (uint256 contributionBeforeFee, , , uint256 discount) = referralGateway.getPrepaidMember(
+            child,
+            tDaoName
+        );
 
-//         assertEq(contributionBeforeFee, couponAmount);
-//         assertEq(discount, 0); // No discount as the coupon is consumed completely and covers the whole membership
-//     }
+        assertEq(contributionBeforeFee, couponAmount);
+        assertEq(discount, 0); // No discount as the coupon is consumed completely and covers the whole membership
+    }
 
-//     //======== coupon equals than contribution ========//
-//     function testCouponPrepaymentCase2() public setCouponPoolAndCouponRedeemer {
-//         uint256 couponAmount = CONTRIBUTION_AMOUNT;
+    //======== coupon equals than contribution ========//
+    function testCouponPrepaymentCase2() public setCouponPoolAndCouponRedeemer {
+        uint256 couponAmount = CONTRIBUTION_AMOUNT;
 
-//         uint256 initialCouponPoolBalance = usdc.balanceOf(couponPool);
-//         uint256 initialOperatorBalance = usdc.balanceOf(operator);
+        uint256 initialCouponPoolBalance = usdc.balanceOf(couponPool);
+        uint256 initialOperatorBalance = usdc.balanceOf(operator);
 
-//         vm.prank(couponRedeemer);
-//         vm.expectEmit(true, true, true, false, address(referralGateway));
-//         emit OnCouponRedeemed(child, tDaoName, couponAmount);
-//         (uint256 feeToOp, ) = referralGateway.payContributionOnBehalfOf(
-//             CONTRIBUTION_AMOUNT,
-//             tDaoName,
-//             address(0),
-//             child,
-//             couponAmount
-//         );
+        vm.prank(couponRedeemer);
+        vm.expectEmit(true, true, true, false, address(referralGateway));
+        emit OnCouponRedeemed(child, tDaoName, couponAmount);
+        (uint256 feeToOp, ) = referralGateway.payContributionOnBehalfOf(
+            CONTRIBUTION_AMOUNT,
+            tDaoName,
+            address(0),
+            child,
+            couponAmount
+        );
 
-//         uint256 finalCouponPoolBalance = usdc.balanceOf(couponPool);
-//         uint256 finalOperatorBalance = usdc.balanceOf(operator);
+        uint256 finalCouponPoolBalance = usdc.balanceOf(couponPool);
+        uint256 finalOperatorBalance = usdc.balanceOf(operator);
 
-//         assertEq(finalCouponPoolBalance, initialCouponPoolBalance - couponAmount);
-//         assertEq(finalOperatorBalance, initialOperatorBalance + feeToOp);
-//         assert(feeToOp > 0);
+        assertEq(finalCouponPoolBalance, initialCouponPoolBalance - couponAmount);
+        assertEq(finalOperatorBalance, initialOperatorBalance + feeToOp);
+        assert(feeToOp > 0);
 
-//         (uint256 contributionBeforeFee, , , uint256 discount) = referralGateway.getPrepaidMember(
-//             child,
-//             tDaoName
-//         );
+        (uint256 contributionBeforeFee, , , uint256 discount) = referralGateway.getPrepaidMember(
+            child,
+            tDaoName
+        );
 
-//         assertEq(contributionBeforeFee, CONTRIBUTION_AMOUNT);
-//         assertEq(discount, 0); // No discount as the coupon is consumed completely and covers the whole membership
-//     }
+        assertEq(contributionBeforeFee, CONTRIBUTION_AMOUNT);
+        assertEq(discount, 0); // No discount as the coupon is consumed completely and covers the whole membership
+    }
 
-//     //======== coupon less than contribution ========//
-//     function testCouponPrepaymentCase3() public setCouponPoolAndCouponRedeemer {
-//         uint256 couponAmount = CONTRIBUTION_AMOUNT;
+    //======== coupon less than contribution ========//
+    function testCouponPrepaymentCase3() public setCouponPoolAndCouponRedeemer {
+        uint256 couponAmount = CONTRIBUTION_AMOUNT;
 
-//         uint256 initialCouponPoolBalance = usdc.balanceOf(couponPool);
-//         uint256 initialOperatorBalance = usdc.balanceOf(operator);
+        uint256 initialCouponPoolBalance = usdc.balanceOf(couponPool);
+        uint256 initialOperatorBalance = usdc.balanceOf(operator);
 
-//         vm.prank(couponRedeemer);
-//         vm.expectEmit(true, true, true, false, address(referralGateway));
-//         emit OnCouponRedeemed(child, tDaoName, couponAmount);
-//         (uint256 feeToOp, ) = referralGateway.payContributionOnBehalfOf(
-//             CONTRIBUTION_AMOUNT * 2,
-//             tDaoName,
-//             address(0),
-//             child,
-//             couponAmount
-//         );
+        vm.prank(couponRedeemer);
+        vm.expectEmit(true, true, true, false, address(referralGateway));
+        emit OnCouponRedeemed(child, tDaoName, couponAmount);
+        (uint256 feeToOp, ) = referralGateway.payContributionOnBehalfOf(
+            CONTRIBUTION_AMOUNT * 2,
+            tDaoName,
+            address(0),
+            child,
+            couponAmount
+        );
 
-//         uint256 finalCouponPoolBalance = usdc.balanceOf(couponPool);
-//         uint256 finalOperatorBalance = usdc.balanceOf(operator);
+        uint256 finalCouponPoolBalance = usdc.balanceOf(couponPool);
+        uint256 finalOperatorBalance = usdc.balanceOf(operator);
 
-//         assertEq(finalCouponPoolBalance, initialCouponPoolBalance - couponAmount);
-//         assertEq(finalOperatorBalance, initialOperatorBalance + feeToOp);
-//         assert(feeToOp > 0);
+        assertEq(finalCouponPoolBalance, initialCouponPoolBalance - couponAmount);
+        assertEq(finalOperatorBalance, initialOperatorBalance + feeToOp);
+        assert(feeToOp > 0);
 
-//         (uint256 contributionBeforeFee, , , uint256 discount) = referralGateway.getPrepaidMember(
-//             child,
-//             tDaoName
-//         );
+        (uint256 contributionBeforeFee, , , uint256 discount) = referralGateway.getPrepaidMember(
+            child,
+            tDaoName
+        );
 
-//         uint256 expectedDiscount = (((CONTRIBUTION_AMOUNT * 2) - couponAmount) *
-//             CONTRIBUTION_PREJOIN_DISCOUNT_RATIO) / 100;
+        uint256 expectedDiscount = (((CONTRIBUTION_AMOUNT * 2) - couponAmount) *
+            CONTRIBUTION_PREJOIN_DISCOUNT_RATIO) / 100;
 
-//         assertEq(contributionBeforeFee, CONTRIBUTION_AMOUNT * 2);
-//         assertEq(discount, expectedDiscount); // Applied to what is left after the coupon
-//     }
+        assertEq(contributionBeforeFee, CONTRIBUTION_AMOUNT * 2);
+        assertEq(discount, expectedDiscount); // Applied to what is left after the coupon
+    }
 
-//     //======== no coupon ========//
-//     function testShouldNootRevertIfThereIsNoCouponPool() public setCouponPoolAndCouponRedeemer {
-//         vm.prank(child);
-//         (uint256 feeToOp, uint256 discount) = referralGateway.payContribution(
-//             CONTRIBUTION_AMOUNT,
-//             tDaoName,
-//             address(0)
-//         );
+    //======== no coupon ========//
+    function testShouldNootRevertIfThereIsNoCouponPool() public setCouponPoolAndCouponRedeemer {
+        vm.prank(child);
+        (uint256 feeToOp, uint256 discount) = referralGateway.payContribution(
+            CONTRIBUTION_AMOUNT,
+            tDaoName,
+            address(0)
+        );
 
-//         assert(feeToOp > 0);
-//         assert(discount > 0);
-//     }
-// }
+        assert(feeToOp > 0);
+        assert(discount > 0);
+    }
+}
