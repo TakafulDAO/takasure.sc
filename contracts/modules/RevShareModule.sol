28aec70c92a5ac07a46cf034c59cf2993631f027
Maikel Ordaz <maikelordaz@gmail.com>
2025-03-11 20:09:31 -0400

    revise: set takasure reserve

--- contracts/modules/RevShareModule.sol
+++ contracts/modules/RevShareModule.sol
@@ -9,6 +9,9 @@
  *      2. It will mint a new NFT per each 250USDC expends by a coupon buyer
  * @dev Upgradeable contract with UUPS pattern
  */
+import {IModuleManager} from "contracts/interfaces/IModuleManager.sol";
+import {IPrejoinModule} from "contracts/interfaces/IPrejoinModule.sol";
+import {ITakasureReserve} from "contracts/interfaces/ITakasureReserve.sol";
 import {UUPSUpgradeable, Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import {ERC721Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
@@ -27,16 +30,28 @@ contract RevShareModule is
     TLDModuleImplementation,
     ERC721Upgradeable
 {
+    IModuleManager private moduleManager;
+    IPrejoinModule private prejoinModule;
+    ITakasureReserve private takasureReserve;
+
     ModuleState private moduleState;
 
-    uint256 public constant TOTAL_SUPPLY = 18_000;
+    bool private prejoinActive;
+
+    event OnTakasureReserveSet(address indexed takasureReserve);
+
+    error RevShareModule__PrejoinStillActive();
 
     /// @custom:oz-upgrades-unsafe-allow-constructor
     constructor() {
         _disableInitializers();
     }
 
-    function initialize(address _operator, address _moduleManager) external initializer {
+    function initialize(
+        address _operator,
+        address _moduleManager,
+        address _prejoinModule
+    ) external initializer {
         AddressAndStates._notZeroAddress(_operator);
         AddressAndStates._notZeroAddress(_moduleManager);
 
@@ -46,6 +61,11 @@ contract RevShareModule is
 
         _grantRole(ModuleConstants.TAKADAO_OPERATOR, _operator);
         _grantRole(ModuleConstants.MODULE_MANAGER, _moduleManager);
+
+        moduleManager = IModuleManager(_moduleManager);
+        prejoinModule = IPrejoinModule(_prejoinModule);
+
+        prejoinActive = true;
     }
 
     /**
@@ -56,6 +76,35 @@ contract RevShareModule is
         ModuleState newState
     ) external override onlyRole(ModuleConstants.MODULE_MANAGER) {
         moduleState = newState;
+    }
+
+    /**
+     * @notice Set the Takasure Reserve contract when deployed
+     */
+    function setTakasureReserve(
+        address _takasureReserve
+    ) external onlyRole(ModuleConstants.TAKADAO_OPERATOR) {
+        AddressAndStates._notZeroAddress(_takasureReserve);
+
+        // To avoid unexpected behavior we need to ensure the prejoin is already disabled
+        require(
+            !moduleManager.isActiveModule(address(prejoinModule)),
+            RevShareModule__PrejoinStillActive()
+        );
+
+        prejoinActive = false;
+        takasureReserve = ITakasureReserve(_takasureReserve);
+
+        emit OnTakasureReserveSet(_takasureReserve);
+    }
+
+    /**
+     * @notice Needed override
+     */
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override(ERC721Upgradeable, AccessControlUpgradeable) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 
     function _authorizeUpgrade(
