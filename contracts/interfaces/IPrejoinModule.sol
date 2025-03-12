219fd425a4f8f792d0e3a9d03fe4e8fc5670422a
Maikel Ordaz <maikelordaz@gmail.com>
2025-03-11 20:08:54 -0400

    chore: new interface

---
+++ contracts/interfaces/IPrejoinModule.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+pragma solidity 0.8.28;
+
+interface IPrejoinModule {
+    function getPrepaidMember(
+        address member
+    )
+        external
+        view
+        returns (
+            uint256 contributionBeforeFee,
+            uint256 contributionAfterFee,
+            uint256 feeToOperator,
+            uint256 discount
+        );
+}
