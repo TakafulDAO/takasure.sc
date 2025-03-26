69d76b6279c4f65ec1e6b3780d9bd7e17ee9b6a0
Maikel Ordaz <maikelordaz@gmail.com>
2025-03-26 19:29:33 -0400

    revise: store new signature

--- contracts/helpers/SignatureStorage.sol
+++ contracts/helpers/SignatureStorage.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.28;
 
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {AddressAndStates} from "contracts/helpers/libraries/checks/AddressAndStates.sol";
 
 contract SignatureStorage is EIP712 {
     mapping(bytes signature => address signer) public signatureToSigner;
@@ -12,6 +13,20 @@ contract SignatureStorage is EIP712 {
     error SignatureStorage__InvalidSigner();
 
     constructor() EIP712("TheLifeDAO", "1") {}
+
+    function storeSignature(
+        bytes calldata signature,
+        bytes32 messageHash,
+        address signer
+    ) external {
+        AddressAndStates._notZeroAddress(signer);
+        require(
+            _isValidSignature(signer, _hashTypedDataV4(messageHash), signature),
+            SignatureStorage__InvalidSigner()
+        );
+        signatureToSigner[signature] = signer;
+        signatureToMessageHash[signature] = messageHash;
+    }
 
     function getMessageHash(bytes32 messageHash) external view returns (bytes32) {
         return _hashTypedDataV4(messageHash);
