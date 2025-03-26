6c7d33777a7d66044ab9c36b7a18e1377dc50bdf
Maikel Ordaz <maikelordaz@gmail.com>
2025-03-26 19:27:48 -0400

    chore: needed imports

--- contracts/helpers/SignatureStorage.sol
+++ contracts/helpers/SignatureStorage.sol
@@ -2,9 +2,13 @@
 
 pragma solidity 0.8.28;
 
+import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+
 contract SignatureStorage is EIP712 {
     mapping(bytes signature => address signer) public signatureToSigner;
     mapping(bytes signature => bytes32 messageHash) public signatureToMessageHash;
 
     error SignatureStorage__InvalidSigner();
+
+    constructor() EIP712("TheLifeDAO", "1") {}
 }
