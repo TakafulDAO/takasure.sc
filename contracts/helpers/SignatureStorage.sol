9c5085a0dafa16ec9d843295d982cdd8ad91d179
Maikel Ordaz <maikelordaz@gmail.com>
2025-03-26 19:29:06 -0400

    revise: validate signature

--- contracts/helpers/SignatureStorage.sol
+++ contracts/helpers/SignatureStorage.sol
@@ -3,6 +3,7 @@
 pragma solidity 0.8.28;
 
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 contract SignatureStorage is EIP712 {
     mapping(bytes signature => address signer) public signatureToSigner;
@@ -14,5 +15,14 @@ contract SignatureStorage is EIP712 {
 
     function getMessageHash(bytes32 messageHash) external view returns (bytes32) {
         return _hashTypedDataV4(messageHash);
+    }
+
+    function _isValidSignature(
+        address _signer,
+        bytes32 _digest,
+        bytes calldata _signature
+    ) internal pure returns (bool) {
+        (address actualSigner, , ) = ECDSA.tryRecover(_digest, _signature);
+        return actualSigner == _signer;
     }
 }
