c28138496be47c0050510c359eb2cf714d3e2a24
Maikel Ordaz <maikelordaz@gmail.com>
2025-03-27 21:15:19 -0400

    revise: new test cases

--- test/unit/06-signatures/SignatureStorageTest.t.sol
+++ test/unit/06-signatures/SignatureStorageTest.t.sol
@@ -7,12 +7,19 @@ import {SignatureStorage} from "contracts/helpers/SignatureStorage.sol";
 
 contract SignatureStorageTest is Test {
     SignatureStorage signatureStorage;
+    address signatureStorageRole = makeAddr("signatureStorageRole");
     address signer;
     uint256 signerPrivKey;
     bytes32 messageHash = keccak256("message");
 
+    event OnSignatureStored(
+        bytes indexed signature,
+        address indexed signer,
+        bytes32 indexed messageHash
+    );
+
     function setUp() public {
-        signatureStorage = new SignatureStorage();
+        signatureStorage = new SignatureStorage(signatureStorageRole);
         (signer, signerPrivKey) = makeAddrAndKey("user");
     }
 
@@ -22,9 +29,17 @@ contract SignatureStorageTest is Test {
         signature_ = abi.encodePacked(r, s, v);
     }
 
+    function testStoreSignatureRevertsIfCallerIsNotAuthorized() public {
+        bytes memory signature = _signMessage(messageHash);
+
+        vm.expectRevert();
+        signatureStorage.storeSignature(signature, messageHash, signer);
+    }
+
     function testStoreSignatureRevertsIfSignerIsAddressZero() public {
         bytes memory signature = _signMessage(messageHash);
 
+        vm.prank(signatureStorageRole);
         vm.expectRevert();
         signatureStorage.storeSignature(signature, messageHash, address(0));
     }
@@ -34,6 +49,7 @@ contract SignatureStorageTest is Test {
 
         address wrongSigner = makeAddr("wrongSigner");
 
+        vm.prank(signatureStorageRole);
         vm.expectRevert(SignatureStorage.SignatureStorage__InvalidSigner.selector);
         signatureStorage.storeSignature(signature, messageHash, wrongSigner);
     }
@@ -41,6 +57,9 @@ contract SignatureStorageTest is Test {
     function testStoreSignature() public {
         bytes memory signature = _signMessage(messageHash);
 
+        vm.prank(signatureStorageRole);
+        vm.expectEmit(true, true, true, false, address(signatureStorage));
+        emit OnSignatureStored(signature, signer, messageHash);
         signatureStorage.storeSignature(signature, messageHash, signer);
 
         assertEq(signatureStorage.signatureToSigner(signature), signer);
