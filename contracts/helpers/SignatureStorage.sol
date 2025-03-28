97f7b90531d5e5b24199c4eb4701bd6d1f75635b
Maikel Ordaz <maikelordaz@gmail.com>
2025-03-27 21:44:53 -0400

    chore: natspec

--- contracts/helpers/SignatureStorage.sol
+++ contracts/helpers/SignatureStorage.sol
@@ -12,12 +12,17 @@ contract SignatureStorage is EIP712, AccessControl {
     mapping(bytes signature => address signer) public signatureToSigner;
     mapping(bytes signature => bytes32 messageHash) public signatureToMessageHash;
 
+    /*//////////////////////////////////////////////////////////////
+                           EVENTS AND ERRORS
+    //////////////////////////////////////////////////////////////*/
+
     event OnSignatureStored(
         bytes indexed signature,
         address indexed signer,
         bytes32 indexed messageHash
     );
 
+    error SignatureStorage__AlreadyStored();
     error SignatureStorage__InvalidSigner();
 
     constructor(address _signatureStorageRole) EIP712("TheLifeDAO", "1") {
@@ -27,11 +32,21 @@ contract SignatureStorage is EIP712, AccessControl {
         _grantRole(SIGNATURE_STORAGE_ROLE, _signatureStorageRole);
     }
 
+    /**
+     * @notice Store the signature and the signer address
+     * @param signature The signature to store, packed as bytes 65 bytes long
+     * @param messageHash The hash of the message that was signed
+     * @param signer The address of the signer
+     * @dev Reverts if the signer is address zero
+     * @dev Reverts if the signature is already stored
+     * @dev The message hash is typed for EIP712
+     */
     function storeSignature(
         bytes calldata signature,
         bytes32 messageHash,
         address signer
     ) external onlyRole(SIGNATURE_STORAGE_ROLE) {
+        require(signatureToSigner[signature] == address(0), SignatureStorage__AlreadyStored());
         require(
             _isValidSignature(signer, _hashTypedDataV4(messageHash), signature),
             SignatureStorage__InvalidSigner()
