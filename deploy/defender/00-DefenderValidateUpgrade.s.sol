e07490113a01cbf3582f58eeba4a6556438d3861
Maikel Ordaz <maikelordaz@gmail.com>
2025-04-14 12:11:56 -0400

    chore: remove comments in script

--- deploy/defender/00-DefenderValidateUpgrade.s.sol
+++ deploy/defender/00-DefenderValidateUpgrade.s.sol
@@ -9,18 +9,7 @@ import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 
 contract DefenderValidateUpgrade is Script {
     function run() external {
-        // ApprovalProcessResponse memory approvalProcess = Defender.getDeployApprovalProcess();
-        // console2.log("Approval process id", approvalProcess.approvalProcessId);
-        // console2.log("Approval process via", approvalProcess.viaType);
-        // string memory relayerKey = "RELAYER_ID";
-
-        // bytes32 salt = keccak256(abi.encodePacked(block.timestamp, msg.sender));
-
         Options memory opts;
-        // opts.defender.salt = salt;
-        // opts.defender.useDefenderDeploy = true;
-        // opts.defender.relayerId = vm.envString(relayerKey);
-        // opts.defender.upgradeApprovalProcessId = approvalProcess.approvalProcessId;
 
         Upgrades.validateUpgrade("PrejoinModule.sol", opts);
 
