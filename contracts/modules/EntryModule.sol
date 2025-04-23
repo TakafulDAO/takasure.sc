ef8c1d4c5e8b1bff6f0dcd61ad8e6fea10ed7d92
Maikel Ordaz <maikelordaz@gmail.com>
2025-04-22 22:03:57 -0400

    revise: refactor constants call

--- contracts/modules/EntryModule.sol
+++ contracts/modules/EntryModule.sol
@@ -60,9 +60,6 @@ contract EntryModule is
     mapping(address parent => mapping(address child => uint256 reward)) public parentRewardsByChild;
     mapping(address parent => mapping(uint256 layer => uint256 reward)) public parentRewardsByLayer;
 
-    uint256 private constant REFERRAL_DISCOUNT_RATIO = 5; // 5% of contribution deducted from contribution
-    uint256 private constant REFERRAL_RESERVE = 5; // 5% of contribution to Referral Reserve
-    bytes32 private constant COUPON_REDEEMER = keccak256("COUPON_REDEEMER");
     bytes32 private constant ROUTER = keccak256("ROUTER");
 
     error EntryModule__NoContribution();
@@ -440,10 +437,10 @@ contract EntryModule is
         uint256 toReferralReserve;
 
         if (_reserve.referralDiscount) {
-            toReferralReserve = (realContribution * REFERRAL_RESERVE) / 100;
+            toReferralReserve = (realContribution * ModuleConstants.REFERRAL_RESERVE) / 100;
 
             if (_parent != address(0)) {
-                discount = ((realContribution - _couponAmount) * REFERRAL_DISCOUNT_RATIO) / 100;
+                discount = ((realContribution - _couponAmount) * ModuleConstants.REFERRAL_DISCOUNT_RATIO) / 100;
 
                 childToParent[_child] = _parent;
 
@@ -818,7 +815,7 @@ contract EntryModule is
 
     function _onlyCouponRedeemerOrCcipReceiver() internal view {
         require(
-            hasRole(COUPON_REDEEMER, msg.sender) || msg.sender == ccipReceiverContract,
+            hasRole(ModuleConstants.COUPON_REDEEMER, msg.sender) || msg.sender == ccipReceiverContract,
             EntryModule__NotAuthorizedCaller()
         );
     }
