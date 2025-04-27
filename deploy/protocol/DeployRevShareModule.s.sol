b89b6d05a34527afdfe9d94068edb8de2ca70ef2
Maikel Ordaz <maikelordaz@gmail.com>
2025-04-27 11:47:10 -0400

    revise: deploy script

---
+++ deploy/protocol/DeployRevShareModule.s.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GNU GPLv3
+
+pragma solidity 0.8.28;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {RevShareModule} from "contracts/modules/RevShareModule.sol";
+import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
+import {HelperConfig} from "deploy/utils/configs/HelperConfig.s.sol";
+import {ModuleManager} from "contracts/modules/manager/ModuleManager.sol";
+
+contract DeployRevShareModule is Script {
+    function run() external returns (address proxy) {
+        uint256 chainId = block.chainid;
+        HelperConfig helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfigByChainId(chainId);
+
+        vm.startBroadcast();
+        proxy = Upgrades.deployUUPSProxy(
+            "RevShareModule.sol",
+            abi.encodeCall(
+                RevShareModule.initialize,
+                (
+                    config.takadaoOperator,
+                    config.kycProvider,
+                    address(new ModuleManager()),
+                    config.contributionToken
+                )
+            )
+        );
+        vm.stopBroadcast();
+    }
+}
