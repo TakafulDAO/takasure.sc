87a558ddbf54577e4216f4a730a9f118a09bda5f
Maikel Ordaz <maikelordaz@gmail.com>
2025-05-28 14:55:27 -0400

    chore: fix some scripts

--- deploy/defender/00-DefenderValidateUpgrade.s.sol
+++ deploy/defender/00-DefenderValidateUpgrade.s.sol
@@ -3,7 +3,7 @@
 pragma solidity 0.8.28;
 
 import {Script, console2} from "forge-std/Script.sol";
-import {PrejoinModule} from "contracts/modules/PrejoinModule.sol";
+import {ReferralGateway} from "contracts/referrals/ReferralGateway.sol";
 import {Defender, Options, ApprovalProcessResponse} from "openzeppelin-foundry-upgrades/Defender.sol";
 import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 
@@ -11,8 +11,8 @@ contract DefenderValidateUpgrade is Script {
     function run() external {
         Options memory opts;
 
-        Upgrades.validateUpgrade("PrejoinModule.sol", opts);
+        Upgrades.validateUpgrade("ReferralGateway.sol", opts);
 
-        console2.log("PrejoinModule.sol is upgradeable");
+        console2.log("ReferralGateway.sol is upgradeable");
     }
 }
