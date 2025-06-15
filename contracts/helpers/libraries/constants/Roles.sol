814dbb98019f7a2440232738c6782e40e7db89c2
Maikel Ordaz <maikelordaz@gmail.com>
2025-06-15 13:28:42 -0400

    chore: update roles

--- contracts/helpers/libraries/constants/Roles.sol
+++ contracts/helpers/libraries/constants/Roles.sol
@@ -7,8 +7,6 @@ library Roles {
     bytes32 internal constant OPERATOR = keccak256("OPERATOR");
     bytes32 internal constant DAO_MULTISIG = keccak256("DAO_MULTISIG");
     bytes32 internal constant KYC_PROVIDER = keccak256("KYC_PROVIDER");
-    bytes32 internal constant MODULE_MANAGER = keccak256("MODULE_MANAGER");
-    bytes32 internal constant ROUTER = keccak256("ROUTER");
     bytes32 internal constant COUPON_REDEEMER = keccak256("COUPON_REDEEMER");
     bytes32 internal constant PAUSE_GUARDIAN = keccak256("PAUSE_GUARDIAN");
 }
