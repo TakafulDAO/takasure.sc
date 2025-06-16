bdbd0036e8221edadee7ed91b8d2a4cd7c4f2a1d
Maikel Ordaz <maikelordaz@gmail.com>
2025-06-16 18:25:02 -0400

    revise: remove token related contracts

--- contracts/interfaces/ITSToken.sol
+++
@@ -1,8 +0,0 @@
-//SPDX-License-Identifier: GNU GPLv3
-
-pragma solidity 0.8.28;
-
-interface ITSToken {
-    function balanceOf(address account) external view returns (uint256);
-    function mint(address to, uint256 amountToMint) external returns (bool);
-}
