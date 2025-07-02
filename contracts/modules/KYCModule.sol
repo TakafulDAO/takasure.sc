10fded83fbbb38e1fc2f524f44e80f7f36648375
Maikel Ordaz <maikelordaz@gmail.com>
2025-07-01 22:01:53 -0400

    revise: manage kyc from subscriptons

--- contracts/modules/KYCModule.sol
+++ contracts/modules/KYCModule.sol
@@ -10,17 +10,18 @@
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ITakasureReserve} from "contracts/interfaces/ITakasureReserve.sol";
 import {ISubscriptionModule} from "contracts/interfaces/ISubscriptionModule.sol";
+import {IReferralRewardsModule} from "contracts/interfaces/IReferralRewardsModule.sol";
 import {IAddressManager} from "contracts/interfaces/IAddressManager.sol";
 
 import {UUPSUpgradeable, Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {ReentrancyGuardTransientUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardTransientUpgradeable.sol";
 import {TLDModuleImplementation} from "contracts/modules/moduleUtils/TLDModuleImplementation.sol";
-import {ReserveAndMemberValuesHook} from "contracts/hooks/ReserveAndMemberValuesHook.sol";
+import {MemberValuesHook} from "contracts/hooks/MemberValuesHook.sol";
 import {MemberPaymentFlow} from "contracts/helpers/payments/MemberPaymentFlow.sol";
 import {ParentRewards} from "contracts/helpers/payments/ParentRewards.sol";
 import {ModuleErrors} from "contracts/helpers/libraries/errors/ModuleErrors.sol";
 
-import {Reserve, Member, MemberState, ModuleState, ProtocolAddress} from "contracts/types/TakasureTypes.sol";
+import {AssociationMember, AssociationMemberState, ModuleState, ProtocolAddress} from "contracts/types/TakasureTypes.sol";
 import {Roles} from "contracts/helpers/libraries/constants/Roles.sol";
 import {TakasureEvents} from "contracts/helpers/libraries/events/TakasureEvents.sol";
 import {AddressAndStates} from "contracts/helpers/libraries/checks/AddressAndStates.sol";
@@ -33,12 +34,16 @@ contract KYCModule is
     UUPSUpgradeable,
     ReentrancyGuardTransientUpgradeable,
     TLDModuleImplementation,
-    ReserveAndMemberValuesHook,
+    MemberValuesHook,
     MemberPaymentFlow,
     ParentRewards
 {
     IAddressManager private addressManager;
     ModuleState private moduleState;
+
+    string public moduleName;
+
+    mapping(address member => bool) public isKYCed; // To check if a member is KYCed
 
     error KYCModule__ContributionRequired();
     error KYCModule__BenefitMultiplierRequestFailed(bytes errorResponse);
@@ -49,11 +54,12 @@ contract KYCModule is
         _disableInitializers();
     }
 
-    function initialize(address _addressManager) external initializer {
+    function initialize(address _addressManager, string calldata _moduleName) external initializer {
         __UUPSUpgradeable_init();
         __ReentrancyGuardTransient_init();
 
         addressManager = IAddressManager(_addressManager);
+        moduleName = _moduleName;
     }
 
     /**
@@ -73,100 +79,29 @@ contract KYCModule is
      * @dev It reverts if the member is already KYCed
      */
     function approveKYC(
-        address memberWallet,
-        uint256 benefitMultiplier
+        address memberWallet
     ) external onlyRole(Roles.KYC_PROVIDER, address(addressManager)) {
         AddressAndStates._onlyModuleState(moduleState, ModuleState.Enabled);
         AddressAndStates._notZeroAddress(memberWallet);
 
-        ITakasureReserve takasureReserve = ITakasureReserve(
-            addressManager.getProtocolAddressByName("TAKASURE_RESERVE").addr
-        );
+        require(!isKYCed[memberWallet], KYCModule__MemberAlreadyKYCed());
 
-        (Reserve memory reserve, Member memory newMember) = _getReserveAndMemberValuesHook(
-            takasureReserve,
+        AssociationMember memory newMember = _getAssociationMembersValuesHook(
+            addressManager,
             memberWallet
         );
-
-        require(!newMember.isKYCVerified, KYCModule__MemberAlreadyKYCed());
-        require(
-            newMember.contribution > 0 && !newMember.isRefunded,
-            KYCModule__ContributionRequired()
-        );
+        require(!newMember.isRefunded, KYCModule__ContributionRequired());
 
         // We update the member values
-        newMember.benefitMultiplier = benefitMultiplier;
-        newMember.membershipStartTime = block.timestamp;
-        newMember.memberState = MemberState.Active; // Active state as the user is already paid the contribution and KYCed
-        newMember.isKYCVerified = true;
-
-        // Then the everyting needed will be updated, proformas, reserves, cash flow,
-        // DRR, BMA, tokens minted, no need to transfer the amounts as they are already paid
-        uint256 credits;
-        (reserve, credits) = _memberPaymentFlow({
-            _contributionBeforeFee: newMember.contribution,
-            _contributionAfterFee: newMember.contribution -
-                ((newMember.contribution * reserve.serviceFee) / 100),
-            _memberWallet: memberWallet,
-            _reserve: reserve,
-            _takasureReserve: takasureReserve
-        });
-
-        newMember.creditsBalance += credits;
+        newMember.memberState = AssociationMemberState.Active; // Active state as the user is already paid the contribution and KYCed
+        isKYCed[memberWallet] = true;
 
         // Reward the parents
-        address parent = childToParent[memberWallet];
-
-        for (uint256 i; i < uint256(MAX_TIER); ++i) {
-            if (parent == address(0)) break;
-
-            uint256 layer = i + 1;
-
-            uint256 parentReward = parentRewardsByChild[parent][memberWallet];
-
-            // Reset the rewards for this child
-            parentRewardsByChild[parent][memberWallet] = 0;
-
-            try IERC20(reserve.contributionToken).transfer(parent, parentReward) {
-                emit TakasureEvents.OnParentRewarded(parent, layer, memberWallet, parentReward);
-            } catch {
-                parentRewardsByChild[parent][memberWallet] = parentReward;
-                emit TakasureEvents.OnParentRewardTransferFailed(
-                    parent,
-                    layer,
-                    memberWallet,
-                    parentReward
-                );
-            }
-            // We update the parent address to check the next parent
-            parent = childToParent[parent];
-        }
+        IReferralRewardsModule(
+            addressManager.getProtocolAddressByName("REFERRAL_REWARDS_MODULE").addr
+        ).rewardParentsFromSubscriptions(memberWallet);
 
         emit TakasureEvents.OnMemberKycVerified(newMember.memberId, memberWallet);
-        emit TakasureEvents.OnMemberJoined(newMember.memberId, memberWallet);
-
-        _setNewReserveAndMemberValuesHook(takasureReserve, reserve, newMember);
-        takasureReserve.memberSurplus(newMember);
-    }
-
-    function _transferContributionToReserve(
-        IERC20 _contributionToken,
-        address _memberWallet,
-        address _takasureReserve,
-        uint256 _contributionAfterFee
-    ) internal override {
-        address subscriptionModuleAddress = addressManager
-            .getProtocolAddressByName("SUBSCRIPTION_MODULE")
-            .addr;
-
-        ISubscriptionModule subscriptionModule = ISubscriptionModule(subscriptionModuleAddress);
-
-        subscriptionModule.transferContributionAfterKyc(
-            _contributionToken,
-            _memberWallet,
-            _takasureReserve,
-            _contributionAfterFee
-        );
     }
 
     ///@dev required by the OZ UUPS module
