426ce6103e8b23a7fde06915d82ae9afbeef72db
Maikel Ordaz <maikelordaz@gmail.com>
2025-07-27 20:32:18 -0400

    chore: fix test

--- test/unit/01-nft/RevShareNftTest.t.sol
+++ test/unit/01-nft/RevShareNftTest.t.sol
@@ -4,21 +4,14 @@ pragma solidity 0.8.28;
 
 import {Test, console2} from "forge-std/Test.sol";
 import {RevShareNFT} from "contracts/tokens/RevShareNFT.sol";
-import {TakasureReserve} from "contracts/core/TakasureReserve.sol";
-import {AddressManager} from "contracts/managers/AddressManager.sol";
 import {UnsafeUpgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 
 contract RevShareNftTest is Test {
-    TakasureReserve takasureReserve;
-    AddressManager addressManager;
     RevShareNFT nft;
     address operator = makeAddr("operator");
     address alice = makeAddr("alice");
 
-    event OnTakasureReserveSet(
-        address indexed oldTakasureReserve,
-        address indexed newTakasureReserve
-    );
+    event OnAddressManagerSet(address indexed oldAddressManager, address indexed newAddressManager);
     event OnBaseURISet(string indexed oldBaseUri, string indexed newBaseURI);
     event OnRevShareNFTMinted(address indexed owner, uint256 tokenId);
     event OnBatchRevShareNFTMinted(
@@ -30,25 +23,12 @@ contract RevShareNftTest is Test {
     function setUp() public {
         string
             memory baseURI = "https://ipfs.io/ipfs/QmQUeGU84fQFknCwATGrexVV39jeVsayGJsuFvqctuav6p/";
-        // nft = new RevShareNFT(operator, baseURI);
         address nftImplementation = address(new RevShareNFT());
         address nftAddress = UnsafeUpgrades.deployUUPSProxy(
             nftImplementation,
-            abi.encodeCall(RevShareNFT.initialize, (operator, baseURI))
+            abi.encodeCall(RevShareNFT.initialize, (baseURI))
         );
         nft = RevShareNFT(nftAddress);
-
-        addressManager = new AddressManager();
-
-        address token = makeAddr("token");
-
-        address takasureReserveImplementation = address(new TakasureReserve());
-        address takasureReserveAddress = UnsafeUpgrades.deployUUPSProxy(
-            takasureReserveImplementation,
-            abi.encodeCall(TakasureReserve.initialize, (token, address(addressManager)))
-        );
-
-        takasureReserve = TakasureReserve(takasureReserveAddress);
     }
 
     /*//////////////////////////////////////////////////////////////
@@ -56,10 +36,8 @@ contract RevShareNftTest is Test {
     //////////////////////////////////////////////////////////////*/
 
     function testNft_initialValues() public view {
-        assertEq(nft.operator(), operator);
-        assertEq(nft.MAX_SUPPLY(), 18_000);
-        assertEq(nft.balanceOf(operator), nft.MAX_SUPPLY());
-        assertEq(nft.totalSupply(), 9_179);
+        assertEq(nft.MAX_SUPPLY(), 8_820);
+        assertEq(nft.totalSupply(), 0);
         assertEq(
             nft.baseURI(),
             "https://ipfs.io/ipfs/QmQUeGU84fQFknCwATGrexVV39jeVsayGJsuFvqctuav6p/"
@@ -70,11 +48,12 @@ contract RevShareNftTest is Test {
                                 SETTERS
     //////////////////////////////////////////////////////////////*/
 
-    function testNft_setTakasureReserve() public {
+    function testNft_setAddressManager() public {
+        address addressManager = makeAddr("addressManager");
         vm.prank(nft.owner());
         vm.expectEmit(true, true, false, false, address(nft));
-        emit OnTakasureReserveSet(address(0), address(takasureReserve));
-        nft.setTakasureReserve(address(takasureReserve));
+        emit OnAddressManagerSet(address(0), address(addressManager));
+        nft.setAddressManager(address(addressManager));
     }
 
     function testNft_takadaoNftsBaseUris() public {
@@ -116,13 +95,6 @@ contract RevShareNftTest is Test {
         assertEq(nft.balanceOf(alice), 1);
     }
 
-    function testNft_callsExternalContract() public {
-        vm.startPrank(nft.owner());
-        nft.setTakasureReserve(address(takasureReserve));
-        nft.mint(alice);
-        vm.stopPrank();
-    }
-
     /*//////////////////////////////////////////////////////////////
                                BATCH MINT
     //////////////////////////////////////////////////////////////*/
@@ -144,11 +116,11 @@ contract RevShareNftTest is Test {
 
         vm.prank(nft.owner());
         vm.expectEmit(true, false, false, false, address(nft));
-        emit OnBatchRevShareNFTMinted(alice, 9179, 9182);
+        emit OnBatchRevShareNFTMinted(alice, 0, 2);
         nft.batchMint(alice, 3);
 
         assertEq(nft.balanceOf(alice), 3);
-        assertEq(nft.totalSupply(), 9182);
+        assertEq(nft.totalSupply(), 2);
     }
 
     /*//////////////////////////////////////////////////////////////
@@ -156,48 +128,8 @@ contract RevShareNftTest is Test {
     //////////////////////////////////////////////////////////////*/
 
     function testNft_transferNftRevertsIfRevShareModuleNotSetUp() public {
-        vm.prank(operator);
+        vm.prank(alice);
         vm.expectRevert(RevShareNFT.RevShareNFT__RevShareModuleNotSetUp.selector);
         nft.transfer(alice, 1);
     }
-
-    // function testNft_transferNft() public {
-    //     assertEq(nft.balanceOf(operator), 18_000);
-    //     assertEq(nft.balanceOf(alice), 0);
-
-    //     vm.prank(operator);
-    //     nft.transfer(alice, 1);
-
-    //     assertEq(nft.balanceOf(operator), 17_999);
-    //     assertEq(nft.balanceOf(alice), 1);
-    // }
-
-    // function testNft_transferFromNft() public {
-    //     address bob = makeAddr("bob");
-    //     address charlie = makeAddr("charlie");
-
-    //     assertEq(nft.balanceOf(operator), 18_000);
-    //     assertEq(nft.balanceOf(alice), 0);
-    //     assertEq(nft.balanceOf(bob), 0);
-    //     assertEq(nft.balanceOf(charlie), 0);
-
-    //     vm.prank(operator);
-    //     nft.transfer(alice, 1);
-
-    //     assertEq(nft.balanceOf(operator), 17_999);
-    //     assertEq(nft.balanceOf(alice), 1);
-    //     assertEq(nft.balanceOf(bob), 0);
-    //     assertEq(nft.balanceOf(charlie), 0);
-
-    //     vm.prank(alice);
-    //     nft.setApprovalForAll(bob, true);
-
-    //     vm.prank(bob);
-    //     nft.transferFrom(alice, charlie, 1);
-
-    //     assertEq(nft.balanceOf(operator), 17_999);
-    //     assertEq(nft.balanceOf(alice), 0);
-    //     assertEq(nft.balanceOf(bob), 0);
-    //     assertEq(nft.balanceOf(charlie), 1);
-    // }
 }
