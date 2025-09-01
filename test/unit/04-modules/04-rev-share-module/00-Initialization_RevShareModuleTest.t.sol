96f131e47afba5bdd1f635cb637393e128c64fab
Maikel Ordaz <maikelordaz@gmail.com>
2025-08-31 22:06:17 -0400

    revise: initialization tests

---
+++ test/unit/04-modules/04-rev-share-module/00-Initialization_RevShareModuleTest.t.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.28;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {TestDeployProtocol} from "test/utils/TestDeployProtocol.s.sol";
+import {HelperConfig} from "deploy/utils/configs/HelperConfig.s.sol";
+import {RevShareModule} from "contracts/modules/RevShareModule.sol";
+import {StdCheats} from "forge-std/StdCheats.sol";
+import {IUSDC} from "test/mocks/IUSDCmock.sol";
+
+contract Initialization_RevShareModuleTest is Test {
+    TestDeployProtocol deployer;
+    RevShareModule revShareModule;
+    HelperConfig helperConfig;
+    address takadao;
+    address revShareModuleAddress;
+
+    function setUp() public {
+        deployer = new TestDeployProtocol();
+        (, , , , , , revShareModuleAddress, , , , helperConfig) = deployer.run();
+
+        revShareModule = RevShareModule(revShareModuleAddress);
+
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfigByChainId(block.chainid);
+
+        takadao = config.takadaoOperator;
+    }
+
+    function testRevShareModule_distributionsAreActive() public {
+        assert(revShareModule.distributionsActive());
+    }
+
+    function testRevShareModule_availableDate() public {
+        assertEq(revShareModule.revenuesAvailableDate(), block.timestamp);
+    }
+
+    function testRevShareModule_nonApprovedDepositsYet() public {
+        assertEq(revShareModule.approvedDeposits(), 0);
+    }
+
+    function testRevShareModule_nonTimeToStopRevenues() public {
+        assertEq(revShareModule.lastTimestampToDistributeRevenues(), 0);
+    }
+
+    function testRevShareModule_noOneHasInteract() public {
+        assertEq(revShareModule.lastUpdateTime(), 0);
+    }
+}
