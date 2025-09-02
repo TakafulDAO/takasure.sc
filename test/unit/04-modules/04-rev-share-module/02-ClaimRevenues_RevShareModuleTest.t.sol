42c84bb152f84f945eb14dad6c5b56c05724c229
Maikel Ordaz <maikelordaz@gmail.com>
2025-09-02 00:52:17 -0400

    chore: test claim setup

--- test/unit/04-modules/04-rev-share-module/02-ClaimRevenues_RevShareModuleTest.t.sol
+++ test/unit/04-modules/04-rev-share-module/02-ClaimRevenues_RevShareModuleTest.t.sol
@@ -8,13 +8,25 @@ import {HelperConfig} from "deploy/utils/configs/HelperConfig.s.sol";
 import {RevShareModule} from "contracts/modules/RevShareModule.sol";
 import {StdCheats} from "forge-std/StdCheats.sol";
 import {IUSDC} from "test/mocks/IUSDCmock.sol";
+import {RevShareNFT} from "contracts/tokens/RevShareNFT.sol";
+import {UnsafeUpgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
+import {AddressManager} from "contracts/managers/AddressManager.sol";
+import {ProtocolAddressType} from "contracts/types/TakasureTypes.sol";
 
 contract ClaimRevenues_RevShareModuleTest is Test {
     TestDeployProtocol deployer;
     RevShareModule revShareModule;
+    RevShareNFT nft;
     HelperConfig helperConfig;
+    IUSDC usdc;
     address takadao;
+    address revenueClaimer;
+    address revenueReceiver;
+    address module = makeAddr("module");
     address revShareModuleAddress;
+    address alice = makeAddr("alice");
+    address bob = makeAddr("bob");
+    address charlie = makeAddr("charlie");
 
     function setUp() public {
         deployer = new TestDeployProtocol();
@@ -25,5 +37,64 @@ contract ClaimRevenues_RevShareModuleTest is Test {
         HelperConfig.NetworkConfig memory config = helperConfig.getConfigByChainId(block.chainid);
 
         takadao = config.takadaoOperator;
+        revenueClaimer = takadao;
+
+        string
+            memory baseURI = "https://ipfs.io/ipfs/QmQUeGU84fQFknCwATGrexVV39jeVsayGJsuFvqctuav6p/";
+        address nftImplementation = address(new RevShareNFT());
+        address nftAddress = UnsafeUpgrades.deployUUPSProxy(
+            nftImplementation,
+            abi.encodeCall(RevShareNFT.initialize, (baseURI, msg.sender))
+        );
+        nft = RevShareNFT(nftAddress);
+
+        uint256 addressManagerAddressSlot = 0;
+        bytes32 addressManagerAddressSlotBytes = vm.load(
+            address(revShareModule),
+            bytes32(uint256(addressManagerAddressSlot))
+        );
+        AddressManager addressManager = AddressManager(
+            address(uint160(uint256(addressManagerAddressSlotBytes)))
+        );
+
+        revenueReceiver = addressManager.getProtocolAddressByName("REVENUE_RECEIVER").addr;
+        usdc = IUSDC(addressManager.getProtocolAddressByName("CONTRIBUTION_TOKEN").addr);
+
+        vm.startPrank(addressManager.owner());
+        addressManager.addProtocolAddress("REVSHARE_NFT", address(nft), ProtocolAddressType.Module);
+        addressManager.addProtocolAddress("RANDOM_MODULE", module, ProtocolAddressType.Module);
+        vm.stopPrank();
+
+        vm.prank(nft.owner());
+        nft.batchMint(alice, 50);
+
+        vm.warp(block.timestamp + 1 days);
+        vm.roll(block.number + 1);
+
+        vm.prank(nft.owner());
+        nft.batchMint(bob, 25);
+
+        vm.warp(block.timestamp + 3 days);
+        vm.roll(block.number + 1);
+
+        vm.prank(nft.owner());
+        nft.batchMint(charlie, 84);
+
+        vm.warp(block.timestamp + 15 days);
+        vm.roll(block.number + 1);
+
+        deal(address(usdc), module, 11_000e6); // 11,000 USDC
+
+        vm.startPrank(module);
+        usdc.approve(address(revShareModule), 11_000e6);
+        revShareModule.notifyNewRevenue(11_000e6);
+        vm.stopPrank();
+
+        uint256 forcedTotalSupply = 1_500;
+        vm.store(
+            address(nft),
+            bytes32(uint256(2)), // slot index for totalSupply
+            bytes32(forcedTotalSupply)
+        );
     }
 }
