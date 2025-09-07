3247042e9bbbb1745b999f1e91993b5f4fee98fb
Maikel Ordaz <maikelordaz@gmail.com>
2025-09-07 06:51:19 -0400

    chore: temporary rearrange

---
+++ test/legacy/unit/core/04_Transfers_TakasureCoreTest.t.sol
@@ -0,0 +1,128 @@
+// // SPDX-License-Identifier: GPL-3.0
+
+// pragma solidity 0.8.28;
+
+// import {Test, console2} from "forge-std/Test.sol";
+// import {TestDeployProtocol} from "test/utils/TestDeployProtocol.s.sol";
+// import {HelperConfig} from "deploy/utils/configs/HelperConfig.s.sol";
+// import {TakasureReserve} from "contracts/core/TakasureReserve.sol";
+// import {SubscriptionModule} from "contracts/modules/SubscriptionModule.sol";
+// import {UserRouter} from "contracts/router/UserRouter.sol";
+// import {StdCheats} from "forge-std/StdCheats.sol";
+// import {BenefitMember, Reserve} from "contracts/types/TakasureTypes.sol";
+// import {IUSDC} from "test/mocks/IUSDCmock.sol";
+// import {IAddressManager} from "contracts/interfaces/managers/IAddressManager.sol";
+
+// contract Transfers_TakasureCoreTest is StdCheats, Test {
+//     TestDeployProtocol deployer;
+//     TakasureReserve takasureReserve;
+//     HelperConfig helperConfig;
+//     SubscriptionModule subscriptionModule;
+//     UserRouter userRouter;
+//     address takasureReserveProxy;
+//     address contributionTokenAddress;
+//     address admin;
+//     address kycService;
+//     address takadao;
+//     address subscriptionModuleAddress;
+//     address userRouterAddress;
+//     IUSDC usdc;
+//     address public alice = makeAddr("alice");
+//     uint256 public constant USDC_INITIAL_AMOUNT = 100e6; // 100 USDC
+//     uint256 public constant CONTRIBUTION_AMOUNT = 25e6; // 25 USDC
+//     uint256 public constant YEAR = 365 days;
+
+//     function setUp() public {
+//         deployer = new TestDeployProtocol();
+//         (
+//             takasureReserveProxy,
+//             ,
+//             subscriptionModuleAddress,
+//             ,
+//             ,
+//             ,
+//             userRouterAddress,
+//             contributionTokenAddress,
+//             ,
+//             helperConfig
+//         ) = deployer.run();
+
+//         subscriptionModule = SubscriptionModule(subscriptionModuleAddress);
+//         userRouter = UserRouter(userRouterAddress);
+
+//         HelperConfig.NetworkConfig memory config = helperConfig.getConfigByChainId(block.chainid);
+
+//         admin = config.daoMultisig;
+//         kycService = config.kycProvider;
+//         takadao = config.takadaoOperator;
+
+//         takasureReserve = TakasureReserve(takasureReserveProxy);
+//         usdc = IUSDC(contributionTokenAddress);
+
+//         // For easier testing there is a minimal USDC mock contract without restrictions
+//         deal(address(usdc), alice, USDC_INITIAL_AMOUNT);
+
+//         vm.startPrank(alice);
+//         usdc.approve(address(subscriptionModule), USDC_INITIAL_AMOUNT);
+//         vm.stopPrank();
+//     }
+
+//     /*//////////////////////////////////////////////////////////////
+//                     JOIN POOL::TRANSFER AMOUNTS
+//     //////////////////////////////////////////////////////////////*/
+
+//     /// @dev Test contribution amount is not transferred to the contract if only the KYC is done
+//     function testTakasureCore_contributionAmountNotTransferToContractWhenKycMissing() public {
+//         uint256 takasureReserveBalanceBefore = usdc.balanceOf(address(takasureReserve));
+//         uint256 subscriptionModuleBalanceBefore = usdc.balanceOf(address(subscriptionModule));
+
+//         vm.prank(alice);
+//         userRouter.paySubscription(address(0), CONTRIBUTION_AMOUNT, (5 * YEAR));
+
+//         uint256 takasureReserveBalanceAfter = usdc.balanceOf(address(takasureReserve));
+//         uint256 subscriptionModuleBalanceAfter = usdc.balanceOf(address(subscriptionModule));
+
+//         assertEq(takasureReserveBalanceAfter, takasureReserveBalanceBefore);
+//         assert(subscriptionModuleBalanceAfter > subscriptionModuleBalanceBefore);
+//     }
+
+//     /// @dev Test contribution amount is transferred to the contract when joins the pool
+//     function testTakasureCore_contributionAmountTransferToContractWhenJoinPool() public {
+//         uint256 takasureReserveBalanceBefore = usdc.balanceOf(address(takasureReserve));
+//         uint256 subscriptionModuleBalanceBefore = usdc.balanceOf(address(subscriptionModule));
+
+//         Reserve memory reserve = takasureReserve.getReserveValues();
+//         uint8 serviceFee = reserve.serviceFee;
+
+//         vm.prank(alice);
+//         userRouter.paySubscription(address(0), CONTRIBUTION_AMOUNT, (5 * YEAR));
+
+//         uint256 takasureReserveBalanceAfter = usdc.balanceOf(address(takasureReserve));
+//         uint256 subscriptionModuleBalanceAfter = usdc.balanceOf(address(subscriptionModule));
+
+//         uint256 fee = (CONTRIBUTION_AMOUNT * serviceFee) / 100;
+//         uint256 deposited = CONTRIBUTION_AMOUNT - fee;
+
+//         assertEq(takasureReserveBalanceAfter, takasureReserveBalanceBefore);
+//         assertEq(subscriptionModuleBalanceAfter, subscriptionModuleBalanceBefore + deposited);
+//     }
+
+//     /// @dev Test service fee is transferred when the member joins the pool
+//     function testTakasureCore_serviceFeeAmountTransferedWhenJoinsPool() public {
+//         Reserve memory reserve = takasureReserve.getReserveValues();
+//         uint8 serviceFee = reserve.serviceFee;
+//         address serviceFeeReceiver = IAddressManager(takasureReserve.addressManager())
+//             .getProtocolAddressByName("FEE_CLAIM_ADDRESS")
+//             .addr;
+//         uint256 serviceFeeReceiverBalanceBefore = usdc.balanceOf(serviceFeeReceiver);
+
+//         vm.prank(alice);
+//         userRouter.paySubscription(address(0), CONTRIBUTION_AMOUNT, (5 * YEAR));
+
+//         uint256 serviceFeeReceiverBalanceAfter = usdc.balanceOf(serviceFeeReceiver);
+
+//         uint256 feeColected = (CONTRIBUTION_AMOUNT * serviceFee) / 100; // 25USDC * 20% = 5USDC
+
+//         assertEq(serviceFeeReceiverBalanceAfter, serviceFeeReceiverBalanceBefore + feeColected);
+//     }
+// }
--- test/legacy/unit/modules/01-subscription-module/00-Reverts_SubscriptionModuleTest.t.sol
+++ test/legacy/unit/modules/01-subscription-module/00-Reverts_SubscriptionModuleTest.t.sol
@@ -9,10 +9,12 @@
 // import {SubscriptionModule} from "contracts/modules/SubscriptionModule.sol";
 // import {UserRouter} from "contracts/router/UserRouter.sol";
 // import {StdCheats} from "forge-std/StdCheats.sol";
-// import {Member, Reserve} from "contracts/types/TakasureTypes.sol";
 // import {IUSDC} from "test/mocks/IUSDCmock.sol";
+// import {ModuleErrors} from "contracts/helpers/libraries/errors/ModuleErrors.sol";
+// import {TakasureEvents} from "contracts/helpers/libraries/events/TakasureEvents.sol";
+// import {AddressAndStates} from "contracts/helpers/libraries/checks/AddressAndStates.sol";
 
-// contract Join_SubscriptionModuleTest is StdCheats, Test {
+// contract Reverts_SubscriptionModuleTest is StdCheats, Test {
 //     TestDeployProtocol deployer;
 //     TakasureReserve takasureReserve;
 //     HelperConfig helperConfig;
@@ -28,9 +30,10 @@
 //     IUSDC usdc;
 //     address public alice = makeAddr("alice");
 //     address public bob = makeAddr("bob");
-//     uint256 public constant USDC_INITIAL_AMOUNT = 1000e6; // 1000 USDC
+//     address public charlie = makeAddr("charlie");
+//     address public david = makeAddr("david");
+//     uint256 public constant USDC_INITIAL_AMOUNT = 150e6; // 100 USDC
 //     uint256 public constant CONTRIBUTION_AMOUNT = 25e6; // 25 USDC
-//     uint256 public constant BENEFIT_MULTIPLIER = 0;
 //     uint256 public constant YEAR = 365 days;
 
 //     function setUp() public {
@@ -63,6 +66,8 @@
 //         // For easier testing there is a minimal USDC mock contract without restrictions
 //         deal(address(usdc), alice, USDC_INITIAL_AMOUNT);
 //         deal(address(usdc), bob, USDC_INITIAL_AMOUNT);
+//         deal(address(usdc), charlie, USDC_INITIAL_AMOUNT);
+//         deal(address(usdc), david, USDC_INITIAL_AMOUNT);
 
 //         vm.startPrank(alice);
 //         usdc.approve(address(subscriptionModule), USDC_INITIAL_AMOUNT);
@@ -70,23 +75,43 @@
 //         vm.startPrank(bob);
 //         usdc.approve(address(subscriptionModule), USDC_INITIAL_AMOUNT);
 //         vm.stopPrank();
+//         vm.startPrank(charlie);
+//         usdc.approve(address(subscriptionModule), USDC_INITIAL_AMOUNT);
+//         vm.stopPrank();
+//         vm.startPrank(david);
+//         usdc.approve(address(subscriptionModule), USDC_INITIAL_AMOUNT);
+//         vm.stopPrank();
 //     }
 
 //     /*//////////////////////////////////////////////////////////////
-//                       JOIN POOL::CREATE NEW MEMBER
+//                                 REVERTS
 //     //////////////////////////////////////////////////////////////*/
 
-
-
-//     function testGasBenchMark_paySubscriptionThroughUserRouter() public {
-//         // Gas used: 505546
+//     /// @dev `paySubscription` must revert if the contribution is less than the minimum threshold
+//     function testSubscriptionModule_paySubscriptionMustRevertIfDepositLessThanMinimum() public {
+//         uint256 wrongContribution = CONTRIBUTION_AMOUNT / 2;
 //         vm.prank(alice);
-//         userRouter.paySubscription(address(0), CONTRIBUTION_AMOUNT, (5 * YEAR));
+//         vm.expectRevert(SubscriptionModule.SubscriptionModule__InvalidContribution.selector);
+//         userRouter.paySubscription(address(0), wrongContribution, (5 * YEAR));
 //     }
 
-//     function testGasBenchMark_paySubscriptionThroughsubscriptionModule() public {
-//         // Gas used: 495580
-//         vm.prank(alice);
-//         subscriptionModule.paySubscription(alice, address(0), CONTRIBUTION_AMOUNT, (5 * YEAR));
+//     /// @dev can not refund someone already refunded
+//     function testSubscriptionModule_refundRevertIfMemberAlreadyRefunded() public {
+//         deal(address(usdc), address(subscriptionModule), 25e6);
+
+//         vm.startPrank(alice);
+//         // Join and refund
+//         userRouter.paySubscription(address(0), CONTRIBUTION_AMOUNT, 5 * YEAR);
+
+//         // 14 days passed
+//         vm.warp(15 days);
+//         vm.roll(block.number + 1);
+
+//         subscriptionModule.refund();
+
+//         // Try to refund again
+//         vm.expectRevert(SubscriptionModule.SubscriptionModule__NothingToRefund.selector);
+//         subscriptionModule.refund();
+//         vm.stopPrank();
 //     }
 // }
