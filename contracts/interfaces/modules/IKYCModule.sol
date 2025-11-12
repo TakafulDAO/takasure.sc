a45c80d40a490567620f24a069ba5a9c880a319d
Maikel Ordaz <maikelordaz@gmail.com>
2025-11-12 00:06:33 -0400

    chore: update KYC module

--- contracts/interfaces/modules/IKYCModule.sol
+++ contracts/interfaces/modules/IKYCModule.sol
@@ -1,7 +1,9 @@
-// SPDX-License-Identifier: GPL-3.0-only
+//SPDX-License-Identifier: GPL-3.0
 
 pragma solidity 0.8.28;
 
 interface IKYCModule {
-    function isKYCed(address user) external view returns (bool);
+    function isKYCed(address member) external view returns (bool);
+    function initialize(address _addressManager, string calldata _moduleName) external;
+    function approveKYC(address memberWallet) external;
 }
