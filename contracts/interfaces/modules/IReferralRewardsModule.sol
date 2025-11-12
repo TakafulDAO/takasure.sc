aa737f25b03071a5d30ed69e95aeae2cde88a0e2
Maikel Ordaz <maikelordaz@gmail.com>
2025-11-12 00:06:54 -0400

    chore: update interface

--- contracts/interfaces/modules/IReferralRewardsModule.sol
+++ contracts/interfaces/modules/IReferralRewardsModule.sol
@@ -3,12 +3,17 @@
 pragma solidity 0.8.28;
 
 interface IReferralRewardsModule {
+    function referralDiscountEnabled() external view returns (bool);
+    function childToParent(address child) external view returns (address);
+    function historicParentRewardsByChild(address parent, address child) external view returns (uint256);
+    function initialize(address _addressManager, string calldata _moduleName) external;
+    function setReferralDiscountState(bool referralDiscountState) external;
     function calculateReferralRewards(
         uint256 contribution,
         uint256 couponAmount,
         address child,
         address parent,
         uint256 feeAmount
-    ) external returns (uint256 newFeeAmount, uint256 discount, uint256 toReferralReserveAmount);
+    ) external returns (uint256 newFeeAmount_, uint256 discount_, uint256 toReferralReserveAmount_);
     function rewardParents(address child) external;
 }
