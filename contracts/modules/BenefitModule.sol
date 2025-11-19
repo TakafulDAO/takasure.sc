244a0218bd5bd50d38c1cdee42584ed56675bd92
Maikel Ordaz <maikelordaz@gmail.com>
2025-11-18 23:00:08 -0400

    revise: Benefit Module

---
+++ contracts/modules/BenefitModule.sol
@@ -0,0 +1,367 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+/**
+ * @title BenefitModule
+ * @author Maikel Ordaz
+ * @notice This contract manage the joins to a specific benefit
+ * @dev This is the implementation for a Beacon Proxy
+ */
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IAddressManager} from "contracts/interfaces/managers/IAddressManager.sol";
+import {ITakasureReserve} from "contracts/interfaces/ITakasureReserve.sol";
+import {ISubscriptionModule} from "contracts/interfaces/modules/ISubscriptionModule.sol";
+import {IKYCModule} from "contracts/interfaces/modules/IKYCModule.sol";
+import {IReferralRewardsModule} from "contracts/interfaces/modules/IReferralRewardsModule.sol";
+import {IProtocolStorageModule} from "contracts/interfaces/modules/IProtocolStorageModule.sol";
+
+import {ModuleImplementation} from "contracts/modules/moduleUtils/ModuleImplementation.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {
+    ReentrancyGuardTransientUpgradeable
+} from "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardTransientUpgradeable.sol";
+
+import {
+    ModuleState,
+    AssociationMember,
+    Reserve,
+    BenefitMember,
+    BenefitMemberState
+} from "contracts/types/TakasureTypes.sol";
+import {CashFlowAlgorithms} from "contracts/helpers/libraries/algorithms/CashFlowAlgorithms.sol";
+import {AddressAndStates} from "contracts/helpers/libraries/checks/AddressAndStates.sol";
+import {ModuleErrors} from "contracts/helpers/libraries/errors/ModuleErrors.sol";
+import {ModuleConstants} from "contracts/helpers/libraries/constants/ModuleConstants.sol";
+import {TakasureEvents} from "contracts/helpers/libraries/events/TakasureEvents.sol";
+import {Roles} from "contracts/helpers/libraries/constants/Roles.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+pragma solidity 0.8.28;
+
+contract BenefitModule is ModuleImplementation, Initializable, ReentrancyGuardTransientUpgradeable {
+    using SafeERC20 for IERC20;
+
+    uint256 private transient normalizedContributionBeforeFee;
+    uint256 private transient feeAmount;
+    uint256 private transient contributionAfterFee;
+    uint256 private transient discount;
+
+    mapping(address member => BenefitMember) private members;
+    mapping(address member => bool) private isMemberCouponALPRedeemer;
+
+    /*//////////////////////////////////////////////////////////////
+                           EVENTS AND ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    event OnMemberJoinedBenefit(string indexed benefitName, uint256 indexed memberId, address indexed member);
+
+    error BenefitModule__BenefitNotSupported();
+    error BenefitModule__InvalidContribution();
+
+    /*//////////////////////////////////////////////////////////////
+                             INITIALIZATION
+    //////////////////////////////////////////////////////////////*/
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address _addressManager, string calldata _moduleName) external initializer {
+        __ReentrancyGuardTransient_init();
+
+        addressManager = IAddressManager(_addressManager);
+        moduleName = _moduleName;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                 JOINS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Called by backend to allow new members to apply to the benefit from the current module
+     * @param memberWallet address of the member
+     * @param contributionBeforeFee in six decimals
+     * @param membershipDuration default 5 years
+     * @param couponAmount amount of the coupon in six decimals
+     * @dev it reverts if the contribution is out of bounds
+     * @dev it reverts if the member is already active
+     * @dev the contribution amount will be round down so the last four decimals will be zero. This means
+     *      that the minimum contribution amount is 0.01 USDC
+     */
+    function joinBenefitOnBehalfOf(
+        address memberWallet,
+        uint256 contributionBeforeFee,
+        uint256 membershipDuration,
+        uint256 couponAmount
+    ) external nonReentrant onlyRole(Roles.BACKEND_ADMIN, address(addressManager)) {
+        // Initial checks and settings
+        (Reserve memory reserve, address parentWallet, AssociationMember memory member) =
+            _paySubscriptionChecksAndsettings(memberWallet, contributionBeforeFee, couponAmount);
+
+        _join(reserve, memberWallet, parentWallet, contributionBeforeFee, membershipDuration, couponAmount);
+
+        if (couponAmount > 0) {
+            isMemberCouponALPRedeemer[memberWallet] = true;
+        }
+
+        _setBenefitInAssociation(member);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                           INTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function _paySubscriptionChecksAndsettings(
+        address _memberWallet,
+        uint256 _contributionBeforeFee,
+        uint256 _couponAmount
+    ) internal returns (Reserve memory reserve_, address parentWallet_, AssociationMember memory member_) {
+        AddressAndStates._onlyModuleState(
+            ModuleState.Enabled,
+            address(this),
+            IAddressManager(addressManager).getProtocolAddressByName("MODULE_MANAGER").addr
+        );
+
+        // Check if the coupon amount is valid
+        require(_couponAmount <= _contributionBeforeFee, ModuleErrors.Module__InvalidCoupon());
+
+        // Check if the member must be KYCed
+        IKYCModule kycModule = IKYCModule(addressManager.getProtocolAddressByName("KYC_MODULE").addr);
+
+        require(kycModule.isKYCed(_memberWallet), ModuleErrors.Module__AddressNotKYCed());
+
+        IProtocolStorageModule protocolStorageModule =
+            IProtocolStorageModule(addressManager.getProtocolAddressByName("PROTOCOL_STORAGE_MODULE").addr);
+
+        // The member can not be part of the corresponding benefit
+        member_ = protocolStorageModule.getAssociationMember(_memberWallet);
+        address[] memory memberBenefits = member_.benefits;
+        uint256 memberBenefitsLength = memberBenefits.length;
+
+        // Check this address is not already part of the member's benefits
+        for (uint256 i; i < memberBenefitsLength; ++i) {
+            require(memberBenefits[i] != address(this), ModuleErrors.Module__AlreadyJoined());
+        }
+
+        ITakasureReserve takasureReserve =
+            ITakasureReserve(addressManager.getProtocolAddressByName("TAKASURE_RESERVE").addr);
+
+        reserve_ = takasureReserve.getReserveValues();
+
+        parentWallet_ = member_.parent;
+
+        _calculateAmountAndFees(_contributionBeforeFee, reserve_.serviceFee);
+    }
+
+    function _calculateAmountAndFees(uint256 _contributionBeforeFee, uint256 _fee) internal {
+        // The minimum we can receive is 0,01 USDC, here we round it. This to prevent rounding errors
+        // i.e. contributionAmount = (25.123456 / 1e4) * 1e4 = 25.12USDC
+        normalizedContributionBeforeFee = (_contributionBeforeFee / ModuleConstants.DECIMAL_REQUIREMENT_PRECISION_USDC)
+            * ModuleConstants.DECIMAL_REQUIREMENT_PRECISION_USDC;
+        feeAmount = (normalizedContributionBeforeFee * _fee) / 100;
+        contributionAfterFee = normalizedContributionBeforeFee - feeAmount;
+    }
+
+    function _join(
+        Reserve memory _reserve,
+        address _memberWallet,
+        address _parentWallet,
+        uint256 _contributionBeforeFee,
+        uint256 _membershipDuration,
+        uint256 _couponAmount
+    ) internal {
+        ITakasureReserve takasureReserve =
+            ITakasureReserve(addressManager.getProtocolAddressByName("TAKASURE_RESERVE").addr);
+
+        BenefitMember memory newBenefitMember = takasureReserve.getMemberFromAddress(_memberWallet);
+
+        // The member must be inactive or canceled to join
+        require(
+            newBenefitMember.memberState == BenefitMemberState.Inactive
+                || newBenefitMember.memberState == BenefitMemberState.Canceled,
+            ModuleErrors.Module__WrongMemberState()
+        );
+
+        // The contribution must be within the limits
+        require(
+            _contributionBeforeFee >= _reserve.minimumThreshold && _contributionBeforeFee <= _reserve.maximumThreshold,
+            BenefitModule__InvalidContribution()
+        );
+
+        // Create a new member
+        uint256 memberId = ++_reserve.memberIdCounter;
+
+        newBenefitMember = _createNewMember({
+            _newMemberId: memberId,
+            _drr: _reserve.dynamicReserveRatio,
+            _membershipDuration: _membershipDuration, // From the input
+            _memberWallet: _memberWallet, // The member wallet
+            _parentWallet: _parentWallet // The parent wallet
+        });
+
+        // Calculate the referral rewards
+        IReferralRewardsModule referralRewardsModule =
+            IReferralRewardsModule(addressManager.getProtocolAddressByName("REFERRAL_REWARDS_MODULE").addr);
+
+        uint256 toReferralReserveAmount;
+        (feeAmount, discount, toReferralReserveAmount) = referralRewardsModule.calculateReferralRewards({
+            contribution: normalizedContributionBeforeFee,
+            couponAmount: _couponAmount,
+            child: _memberWallet,
+            parent: _parentWallet,
+            feeAmount: feeAmount
+        });
+
+        // We run the model algorithms to update the reserve values, proformas, cash flow,
+        // DRR, BMA, tokens minted, no need to transfer the amounts as they are already paid
+
+        _reserve = CashFlowAlgorithms._updateNewReserveValues({
+            _takasureReserve: takasureReserve,
+            _contributionAfterFee: newBenefitMember.contribution
+                - ((newBenefitMember.contribution * _reserve.serviceFee) / 100),
+            _contributionBeforeFee: newBenefitMember.contribution,
+            _reserve: _reserve
+        });
+
+        // Set the member values
+        newBenefitMember.discount = discount;
+        newBenefitMember.creditsBalance += _contributionBeforeFee;
+
+        // Store the member as part of this benefit
+        members[_memberWallet] = newBenefitMember;
+
+        _performTransfers({
+            _memberWallet: _memberWallet,
+            _couponAmount: _couponAmount,
+            _toReferralReserveAmount: toReferralReserveAmount,
+            _referralRewardsModule: address(referralRewardsModule)
+        });
+
+        // Reward parents
+        referralRewardsModule.rewardParents({child: _memberWallet});
+        emit OnMemberJoinedBenefit(moduleName, newBenefitMember.memberId, _memberWallet);
+
+        // Update the reserve with the new member
+        takasureReserve.setReserveValuesFromModule(_reserve);
+        takasureReserve.setMemberValuesFromModule(newBenefitMember);
+        takasureReserve.memberSurplus(newBenefitMember);
+    }
+
+    function _setBenefitInAssociation(AssociationMember memory _member) internal {
+        IProtocolStorageModule protocolStorageModule =
+            IProtocolStorageModule(addressManager.getProtocolAddressByName("PROTOCOL_STORAGE_MODULE").addr);
+        address[] memory newBenefits = new address[](_member.benefits.length + 1);
+        uint256 currentBenefitsLength = _member.benefits.length;
+
+        for (uint256 i; i < currentBenefitsLength; ++i) {
+            newBenefits[i] = _member.benefits[i];
+        }
+
+        newBenefits[currentBenefitsLength] = address(this);
+        _member.benefits = newBenefits;
+
+        protocolStorageModule.updateAssociationMember(_member);
+    }
+
+    function _createNewMember(
+        uint256 _newMemberId,
+        uint256 _drr,
+        uint256 _membershipDuration,
+        address _memberWallet,
+        address _parentWallet
+    ) internal returns (BenefitMember memory) {
+        uint256 claimAddAmount =
+            ((normalizedContributionBeforeFee - feeAmount) * (100 - _drr)) / 100;
+
+        BenefitMember memory newMember = BenefitMember({
+            memberId: _newMemberId,
+            benefitMultiplier: 0, // Placeholder, will be set after
+            membershipDuration: _membershipDuration,
+            membershipStartTime: block.timestamp,
+            lastPaidYearStartDate: block.timestamp,
+            contribution: normalizedContributionBeforeFee,
+            discount: discount,
+            claimAddAmount: claimAddAmount,
+            totalContributions: normalizedContributionBeforeFee,
+            totalServiceFee: feeAmount,
+            creditsBalance: 0,
+            wallet: _memberWallet,
+            parent: _parentWallet,
+            memberState: BenefitMemberState.Active,
+            memberSurplus: 0,
+            lastEcr: 0,
+            lastUcr: 0
+        });
+
+        emit TakasureEvents.OnMemberCreated(
+            newMember.memberId,
+            _memberWallet,
+            normalizedContributionBeforeFee,
+            feeAmount,
+            _membershipDuration,
+            block.timestamp
+        );
+
+        return newMember;
+    }
+
+    /**
+     * @notice This function will perform the transfers needed when a member joins a benefit
+     * @dev The transfers are:
+     *      - The subscription amount minus fees. SubscriptionModule -> TakasureReserve
+     *      - The contribution minus subscription and fees (only if needed). Member -> TakasureReserve
+     *      - The service fee amount (only if needed). Member -> FeeClaimAddress
+     *      - The coupon amount (only if needed).  Coupon pool -> TakasureReserve
+     *      - The referral rewards amount (only if needed). Member -> ReferralRewardsModule
+     */
+    function _performTransfers(
+        address _memberWallet,
+        uint256 _couponAmount,
+        uint256 _toReferralReserveAmount,
+        address _referralRewardsModule
+    ) internal {
+        // First call the subscription module to transfer the subscription amount to the reserves
+        uint256 subscription = ISubscriptionModule(addressManager.getProtocolAddressByName("SUBSCRIPTION_MODULE").addr)
+            .transferSubscriptionToReserve(_memberWallet);
+
+        uint256 _amountToTransferFromMember;
+
+        if (_couponAmount > 0) {
+            _amountToTransferFromMember = contributionAfterFee - subscription - discount - _couponAmount;
+        } else {
+            _amountToTransferFromMember = contributionAfterFee - subscription - discount;
+        }
+
+        IERC20 contributionToken = IERC20(addressManager.getProtocolAddressByName("CONTRIBUTION_TOKEN").addr);
+
+        // Transfer to reserve
+        if (_amountToTransferFromMember > 0) {
+            contributionToken.safeTransferFrom(
+                _memberWallet,
+                addressManager.getProtocolAddressByName("TAKASURE_RESERVE").addr,
+                _amountToTransferFromMember
+            );
+
+            // Transfer the service fee to the fee claim address
+            address feeClaimAddress = addressManager.getProtocolAddressByName("FEE_CLAIM_ADDRESS").addr;
+
+            contributionToken.safeTransferFrom(_memberWallet, feeClaimAddress, feeAmount);
+        }
+
+        // Transfer the coupon amount to this contract
+        if (_couponAmount > 0) {
+            address couponPool = addressManager.getProtocolAddressByName("COUPON_POOL").addr;
+            contributionToken.safeTransferFrom(couponPool, address(this), _couponAmount);
+        }
+
+        // Lastly, transfer the corresponding amount to the referral reserve
+        if (_toReferralReserveAmount > 0) {
+            contributionToken.safeTransferFrom(_memberWallet, _referralRewardsModule, _toReferralReserveAmount);
+        }
+    }
+
+    function _stringsEqual(string memory _a, string memory _b) internal pure returns (bool) {
+        return bytes(_a).length == bytes(_b).length && keccak256(bytes(_a)) == keccak256(bytes(_b));
+    }
+}
