e7e80765a5b17a567fd2983b8dfaefd526aed5d8
Maikel Ordaz <maikelordaz@gmail.com>
2025-11-30 17:30:52 -0400

    revise: implement vault

--- contracts/saveFunds/SFVault.sol
+++ contracts/saveFunds/SFVault.sol
@@ -8,12 +8,18 @@
  */
 
 // todo: access control, maybe write this as a module with the other ones?
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import {UUPSUpgradeable, Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {ERC4626Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC4626Upgradeable.sol";
+
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 pragma solidity 0.8.28;
 
-contract SFVault is Initializable, UUPSUpgradeable {
+contract SFVault is Initializable, UUPSUpgradeable, ERC4626Upgradeable {
+    using SafeERC20 for IERC20;
+
     /*//////////////////////////////////////////////////////////////
                              INITIALIZATION
     //////////////////////////////////////////////////////////////*/
@@ -23,8 +29,9 @@ contract SFVault is Initializable, UUPSUpgradeable {
         _disableInitializers();
     }
 
-    function initialize() external initializer {
+    function initialize(IERC20 _underlying) external initializer {
         __UUPSUpgradeable_init();
+        __ERC4626_init(_underlying);
     }
 
     /*//////////////////////////////////////////////////////////////
