8d6d8b7c05e9470d565f112c6b0470166d9b8142
Maikel Ordaz <maikelordaz@gmail.com>
2025-12-02 21:00:38 -0400

    chore: deploy scripts

---
+++ test/utils/05-DeploySFVault.s.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.28;
+
+import {Script, console2, stdJson} from "forge-std/Script.sol";
+import {SFVault} from "contracts/saveFunds/SFVault.sol";
+import {HelperConfig} from "deploy/utils/configs/HelperConfig.s.sol";
+import {UnsafeUpgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract DeploySFVault is Script {
+    function run() external returns (HelperConfig.NetworkConfig memory config, SFVault sfVault) {
+        HelperConfig helperConfig = new HelperConfig();
+        config = helperConfig.getConfigByChainId(block.chainid);
+
+        vm.startBroadcast(msg.sender);
+
+        // Deploy SFVault
+        address sfVaultImplementation = address(new SFVault());
+
+        address sfVaultAddress = UnsafeUpgrades.deployUUPSProxy(
+            sfVaultImplementation,
+            abi.encodeCall(SFVault.initialize, (IERC20(config.contributionToken), "SF Vault", "SFV"))
+        );
+
+        sfVault = SFVault(sfVaultAddress);
+
+        vm.stopBroadcast();
+
+        return (config, sfVault);
+    }
+
+    // To avoid this contract to be count in coverage
+    function test() external {}
+}
