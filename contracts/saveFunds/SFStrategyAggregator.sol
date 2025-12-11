c07b94ade59065b279ad67d2a6a502a7713a2732
Maikel Ordaz <maikelordaz@gmail.com>
2025-12-11 18:11:54 -0400

    revise: initial skeleton

---
+++ contracts/saveFunds/SFStrategyAggregator.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+/**
+ * @title SFStrategyAggregator
+ * @author Maikel Ordaz
+ * @notice Multi strategy aggregator for SaveFunds vaults.
+ */
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ISFStrategy} from "contracts/interfaces/saveFunds/ISFStrategy.sol";
+import {IAddressManager} from "contracts/interfaces/managers/IAddressManager.sol";
+
+import {UUPSUpgradeable, Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {
+    ReentrancyGuardTransientUpgradeable
+} from "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardTransientUpgradeable.sol";
+import {PausableUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
+
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+pragma solidity 0.8.28;
+
+contract SFStrategyAggregator is
+    Initializable,
+    UUPSUpgradeable,
+    ReentrancyGuardTransientUpgradeable,
+    PausableUpgradeable
+{
+    using SafeERC20 for IERC20;
+
+    IAddressManager public addressManager;
+
+    /*//////////////////////////////////////////////////////////////
+                             INITIALIZATION
+    //////////////////////////////////////////////////////////////*/
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(IAddressManager _addressManager) external initializer {
+        __UUPSUpgradeable_init();
+        __ReentrancyGuardTransient_init();
+        __Pausable_init();
+
+        addressManager = _addressManager;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                SETTERS
+    //////////////////////////////////////////////////////////////*/
+
+    // todo: access control
+    function pause() external {
+        _pause();
+    }
+
+    // todo: access control
+    function unpause() external {
+        _unpause();
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                           INTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    ///@dev required by the OZ UUPS module.
+    // todo: access control
+    function _authorizeUpgrade(address newImplementation) internal override {}
+}
