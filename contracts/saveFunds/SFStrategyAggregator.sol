0e456abb82fff37c027078cc16a59f60f90332d6
Maikel Ordaz <maikelordaz@gmail.com>
2025-12-12 21:44:16 -0400

    revise: some getters

--- contracts/saveFunds/SFStrategyAggregator.sol
+++ contracts/saveFunds/SFStrategyAggregator.sol
@@ -21,6 +21,7 @@ import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol
 pragma solidity 0.8.28;
 
 contract SFStrategyAggregator is
+    ISFStrategy,
     Initializable,
     UUPSUpgradeable,
     ReentrancyGuardTransientUpgradeable,
@@ -29,6 +30,18 @@ contract SFStrategyAggregator is
     using SafeERC20 for IERC20;
 
     IAddressManager public addressManager;
+
+    IERC20 private underlying;
+
+    struct SubStrategy {
+        ISFStrategy strategy;
+        uint16 targetWeightBps; // 0 to 10000 (0% to 100%)
+        bool isActive;
+    }
+
+    SubStrategy[] private subStrategies;
+
+    uint256 public maxTVL;
 
     /*//////////////////////////////////////////////////////////////
                              INITIALIZATION
@@ -39,12 +52,15 @@ contract SFStrategyAggregator is
         _disableInitializers();
     }
 
-    function initialize(IAddressManager _addressManager) external initializer {
+    function initialize(IAddressManager _addressManager, IERC20 _asset, uint256 _maxTVL) external initializer {
         __UUPSUpgradeable_init();
         __ReentrancyGuardTransient_init();
         __Pausable_init();
 
         addressManager = _addressManager;
+
+        underlying = _asset;
+        maxTVL = _maxTVL;
     }
 
     /*//////////////////////////////////////////////////////////////
@@ -62,10 +78,57 @@ contract SFStrategyAggregator is
     }
 
     /*//////////////////////////////////////////////////////////////
+                                GETTERS
+    //////////////////////////////////////////////////////////////*/
+
+    function asset() external view returns (address) {
+        return address(underlying);
+    }
+
+    function totalAssets() public view override returns (uint256) {
+        uint256 sum = underlying.balanceOf(address(this)); // iddle inside aggregator
+
+        uint256 len = subStrategies.length;
+
+        for (uint256 i; i < len; ++i) {
+            SubStrategy memory strat = subStrategies[i];
+
+            if (!strat.isActive) continue;
+
+            sum += strat.strategy.totalAssets();
+        }
+
+        return sum;
+    }
+
+    function maxDeposit() external view override returns (uint256) {
+        // 0 means no cap
+        if (maxTVL == 0) return type(uint256).max;
+
+        uint256 current = totalAssets();
+        if (current >= maxTVL) return 0;
+        return maxTVL - current;
+    }
+
+    function maxWithdraw() external view override returns (uint256) {
+        // todo: check
+        return totalAssets();
+    }
+
+    /*//////////////////////////////////////////////////////////////
                            INTERNAL FUNCTIONS
     //////////////////////////////////////////////////////////////*/
 
     ///@dev required by the OZ UUPS module.
     // todo: access control
     function _authorizeUpgrade(address newImplementation) internal override {}
+
+    // todo: implement the next functions, written here for now for compilation issues as I'm inheriting ISFStrategy
+    function withdraw(uint256, address, bytes calldata) external returns (uint256) {}
+    function vault() external view returns (address) {}
+    function setMaxTVL(uint256) external {}
+    function setKeeper(address) external {}
+    function setConfig(bytes calldata) external {}
+    function emergencyExit(address) external {}
+    function deposit(uint256, bytes calldata) external returns (uint256) {}
 }
