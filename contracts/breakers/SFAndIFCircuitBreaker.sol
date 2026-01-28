0b2b89c0fdf33a61583c1f57588604079eeb3838
Maikel Ordaz <maikelordaz@gmail.com>
2026-01-28 15:18:06 -0400

    chore: move types

--- contracts/breakers/SFAndIFCircuitBreaker.sol
+++ contracts/breakers/SFAndIFCircuitBreaker.sol
@@ -25,6 +25,7 @@ import {
 } from "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardTransientUpgradeable.sol";
 
 import {Roles} from "contracts/helpers/libraries/constants/Roles.sol";
+import {GuardConfig, Window, RequestKind, WithdrawalRequest} from "contracts/types/CircuitBreakerTypes.sol";
 import {VaultsPauseFlags} from "contracts/helpers/libraries/flags/VaultsPauseFlags.sol";
 
 contract SFAndIFCircuitBreaker is Initializable, UUPSUpgradeable, ReentrancyGuardTransientUpgradeable {
@@ -38,56 +39,6 @@ contract SFAndIFCircuitBreaker is Initializable, UUPSUpgradeable, ReentrancyGuar
     mapping(uint256 => WithdrawalRequest) public withdrawalRequests;
     mapping(address vault => bool) public tripped; // Whether a vault has been triggered/tripped at least once (informational).
     mapping(address vault => uint256) public pauseFlags; // Flags per vault
-
-    /*//////////////////////////////////////////////////////////////
-                                TYPES
-    //////////////////////////////////////////////////////////////*/
-
-    // TODO: maybe move it to other types later
-
-    // Configuration for a protected vault.
-    struct GuardConfig {
-        // Rolling 24h window caps (assets). 0 disables the cap.
-        uint256 globalWithdrawCap24hAssets;
-        uint256 userWithdrawCap24hAssets;
-
-        // Large-withdraw approval threshold (assets). 0 disables approvals.
-        uint256 approvalThresholdAssets;
-
-        // Whether the vault is protected/enabled.
-        bool enabled;
-    }
-
-    // Rolling 24h accumulator window.
-    struct Window {
-        uint64 start;
-        uint256 withdrawn;
-    }
-
-    enum RequestKind {
-        Withdraw, // request originated from withdraw(assets)
-        Redeem // request originated from redeem(shares)
-    }
-
-    // Large-withdraw request created by the vault hook.
-    // TODO: No timelock in v1. Must be added for the Investment Fund use case in future versions.
-    struct WithdrawalRequest {
-        address vault;
-        address owner;
-        address receiver;
-
-        RequestKind kind;
-
-        // What the user attempted at request time:
-        uint256 assetsRequested; // for Withdraw: exact; for Redeem: estimated via previewRedeem
-        uint256 sharesRequested; // for Redeem: exact; for Withdraw: estimated via previewWithdraw
-
-        // Lifecycle
-        uint64 createdAt;
-        bool approved;
-        bool executed;
-        bool cancelled;
-    }
 
     /*//////////////////////////////////////////////////////////////
                            EVENTS AND ERRORS
