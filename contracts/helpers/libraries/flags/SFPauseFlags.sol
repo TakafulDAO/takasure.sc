0da2bddd97844c91463ee74da761fe89b67968a9
Maikel Ordaz <maikelordaz@gmail.com>
2026-01-28 00:33:22 -0400

    revise: pause flags

---
+++ contracts/helpers/libraries/flags/SFPauseFlags.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.28;
+
+library SFPauseflags {
+    bytes1 internal constant LARGE_WITHDRAW_QUEUED_FLAG = 0x00;
+    bytes1 internal constant RATE_LIMIT_EXCEEDED_FLAG = 0x01;
+    bytes1 internal constant EXECUTE_INVALID_STATE = 0x02;
+}
