a2f5871f3f17e2e2629a91c0fb73649141f3c6be
Maikel Ordaz <maikelordaz@gmail.com>
2026-01-28 15:01:16 -0400

    chore: comments

--- contracts/helpers/libraries/flags/VaultsPauseFlags.sol
+++ contracts/helpers/libraries/flags/VaultsPauseFlags.sol
@@ -2,8 +2,10 @@
 pragma solidity 0.8.28;
 
 library VaultsPauseFlags {
-    // Each flag is a single bit. Can use more than one flag with `|` and test them with `&`.
+    // Each flag is a single bit. Can use more than one flag with `|`.
     uint256 internal constant RATE_LIMIT_EXCEEDED_FLAG = 1 << 0; // 0b...0001
     uint256 internal constant LARGE_WITHDRAW_QUEUED_FLAG = 1 << 1; // 0b...0010
     uint256 internal constant EXECUTE_INVALID_STATE = 1 << 2; // 0b...0100
+
+    // Add new flags as needed
 }
