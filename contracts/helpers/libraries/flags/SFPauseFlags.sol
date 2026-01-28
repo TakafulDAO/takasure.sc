5fb1ab29eb501e58cf6c740e7b7557605f373125
Maikel Ordaz <maikelordaz@gmail.com>
2026-01-28 00:54:09 -0400

    chore: reorder

--- contracts/helpers/libraries/flags/SFPauseFlags.sol
+++ contracts/helpers/libraries/flags/SFPauseFlags.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.28;
 
 library SFPauseflags {
-    bytes1 internal constant LARGE_WITHDRAW_QUEUED_FLAG = 0x01;
-    bytes1 internal constant RATE_LIMIT_EXCEEDED_FLAG = 0x02;
+    bytes1 internal constant RATE_LIMIT_EXCEEDED_FLAG = 0x01;
+    bytes1 internal constant LARGE_WITHDRAW_QUEUED_FLAG = 0x02;
     bytes1 internal constant EXECUTE_INVALID_STATE = 0x03;
 }
