6fce07321c2d1627928c6bf2c5ddafff4801c981
Maikel Ordaz <maikelordaz@gmail.com>
2026-02-06 00:36:17 -0400

    revise: uni v3 strategy getters

---
+++ contracts/interfaces/saveFunds/ISFUniswapV3StrategyLens.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+pragma solidity 0.8.28;
+
+interface ISFUniswapV3StrategyLens {
+    function asset() external view returns (address);
+    function pool() external view returns (address);
+    function otherToken() external view returns (address);
+    function vault() external view returns (address);
+    function paused() external view returns (bool);
+    function positionTokenId() external view returns (uint256);
+    function tickLower() external view returns (int24);
+    function tickUpper() external view returns (int24);
+    function twapWindow() external view returns (uint32);
+    function totalAssets() external view returns (uint256);
+}
