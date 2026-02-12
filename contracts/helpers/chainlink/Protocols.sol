879c62f3577cfd08d1c63f793de60a35f558198a
Maikel Ordaz <maikelordaz@gmail.com>
2026-02-12 00:12:08 -0400

    revise: protocols library

---
+++ contracts/helpers/chainlink/Protocols.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: GNU GPLv3
+pragma solidity 0.8.28;
+
+/**
+ * @title Protocols
+ * @author Maikel Ordaz
+ * @notice Protocols compatible with CCIP.
+ */
+
+library Protocols {
+    uint256 constant SAVE_VAULT = 0x01;
+    uint256 constant INVEST_VAULT = 0x02;
+}
